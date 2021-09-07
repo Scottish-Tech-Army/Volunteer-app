226b051faf957a81ab4b06ba4cacddfdac9acee2
Stephen Ramsay <thestephenramsay@gmail.com>
2021-09-07 11:22:51 +0100

    Console log to see data coming through to client

--- api/app.js
+++ api/app.js
@@ -1,9 +1,24 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
 
+app.use(cors());
+
 app.get("/", function (req, res) {
-  res.json({ message: "Hello World" });
+  res.json({
+    fields: {
+      project: {
+        key: "TEST",
+      },
+      summary: "Some data from the express server",
+      description: "Creating an issue while setting custom field values",
+      issuetype: {
+        name: "Bug",
+      },
+      customfield_11050: "Free text!.",
+    },
+  });
 });
 
 app.listen(5000, function (req, res) {
--- client/App.js
+++ client/App.js
@@ -1,21 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useEffect, useState } from "react";
+import {
+  ActivityIndicator,
+  FlatList,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  const [isLoading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
+
+  const getJsonFromExpress = async () => {
+    try {
+      const response = await fetch("http://localhost:5000");
+      const json = await response.json();
+      setData(json.fields);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getJsonFromExpress();
+  }, []);
+
+  console.log(data);
+
+  return null;
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
