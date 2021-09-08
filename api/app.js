69f96ddca6c1e95ac00fb732730bed335483cf29
hamid <hamidq88@gmail.com>
2021-09-08 22:30:13 +0100

    express api updated to include initial REST jira api

--- api/app.js
+++ api/app.js
@@ -1,9 +1,33 @@
 import express from "express";
+import fetch from "node-fetch";
 
 const app = express();
 
-app.get("/", function (req, res) {
-  res.json({ message: "Hello Pluto" });
+app.get("/", function (req, res, next) {
+  
+  // const fetch = require('node-fetch');
+
+  fetch('https://sta2020.atlassian.net/rest/api/3/project/search', {
+    method: 'GET',
+    headers: {
+      'Authorization': `Basic ${Buffer.from(
+        // below use email address you used for jira and generate token from jira
+        'example@hotmail.com:<your token>'
+      ).toString('base64')}`,
+      'Accept': 'application/json'
+    }
+  })
+  .then(response => {
+    console.log(
+      `Response: ${response.status} ${response.statusText}`
+    );
+    if(response.status >= 200 && response.status < 300){
+      return response.json();
+    }
+    return Promise.reject(new Error(response.statusText));
+  })
+  .then(json => res.json(json))
+  .catch(err => next(err));
 });
 
 app.listen(5000, function (req, res) {
