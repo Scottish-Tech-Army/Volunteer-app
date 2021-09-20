2d8f1c926fa1a0b33901825b7b38e6783325482b
Andrew Sharkey <sharkand@hotmail.com>
2021-09-20 17:47:40 +0100

    spike/andrew_data_hookup - Fixed issue with missing styled-components dependency.

--- client/App.js
+++ client/App.js
@@ -2,9 +2,25 @@ import React, {useState, useEffect} from 'react';
 import {Text, SafeAreaView} from 'react-native';
 import Theme from './styles/Theme'
 import ProjectList from './screens/ProjectList';
-import info from '../api/data.js';
+//import {info} from '../api/data.js';
 
 export default function App() {
+
+  const info = 
+  {projects: [
+      {
+          key: "SVA",
+          name: "STA-volunteer App",
+          type: "Team-managed software",
+          client: "STA - internal",
+          role: "Lead developer",
+          description: "The lead tester is a coordination and management role, so an understanding of and experience in a number of testing disciplines, rather than depth in any specific one.",
+          skills: ["React Native", "Node.js"],
+          hours: "5-10 hours per week",
+          required: "One person",
+          buddying: true
+      }
+  ]}
 
   const [data, setData] = useState(info.projects);
 
