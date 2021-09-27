b6a5224a7fec9e39a85fdb9eab720ad412ee1cef
Andrew Sharkey <sharkand@hotmail.com>
2021-09-27 18:43:58 +0100

    Changed test to one that should work with @testing-library/react-native.  However, the latter package  won't install.

--- client/App.test.js
+++ client/App.test.js
@@ -3,9 +3,24 @@ import renderer from 'react-test-renderer';
 
 import App from './App';
 
-describe('<App />', () => {
-  it('has 5 children', () => {
-    const tree = renderer.create(<App />).toJSON();
-    expect(tree.children.length).toBe(5);
+// describe('<App />', () => {
+//   it('has 5 children', () => {
+//     const tree = renderer.create(<App />).toJSON();
+//     expect(tree.children.length).toBe(5);
+//   });
+// });
+
+describe('App component', () => {
+  it('shout render default opportunity list', () => {
+    const { getByText, toJSON } = render(<App />);
+    expect(getByText('All projects')).toBeDefined();
+    expect(getByText('Saved Projects')).toBeDefined();
+    expect(getByText('My Projects')).toBeDefined();
+    expect(getByText('Project Title')).toBeDefined();
+    expect(getByText('STA internal')).toBeDefined();
+    expect(getByText('Lead Test Analyst')).toBeDefined();
+    expect(getByText(/The Lead Tester is a/)).toBeDefined();
+    // TODO test interaction
+    expect(toJSON()).toMatchSnapshot();
   });
 });
