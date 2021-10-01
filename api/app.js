62ca7fcdd47c81e451b568378c20d4f27f44791e
Lorne Stalker <lorne.stalker@gmail.com>
2021-10-01 17:25:06 +0100

    setting up folder structure and router

--- api/app.js
+++ api/app.js
@@ -4,17 +4,17 @@ const routes = require('./routes');
 
 app.use('/', routes);
 
-app.use((req, res, next) => {
-  const err = new Error("Something went wrong. Please try again.");
-  err.status = 500;
-  next(err);
-});
+// app.use((req, res, next) => {
+//   const err = new Error("Something went wrong. Please try again.");
+//   err.status = 500;
+//   next(err);
+// });
 
-app.use((err, req, res, next) => {
-  const notFound = new Error('Resource Not Found');
-  notFound.status = 404;
-  next(notFound);
-});
+// app.use((err, req, res, next) => {
+//   const notFound = new Error('Resource Not Found');
+//   notFound.status = 404;
+//   next(notFound);
+// });
 
 app.listen(5000, function (req, res) {
   console.log("App running on on locatlhost:5000");
--- api/routes/projects.js
+++ api/routes/projects.js
@@ -1,11 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const data = require('../data');
-const jiraProjectHelper = require('../jira-helpers/projects')
 
 router.get('/', (req, res) => {
-    let returnedData = jiraProjectHelper.getProjects();
-    res.json(returnedData);
+    res.json(data);
 });
 
 module.exports = router;
