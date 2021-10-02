acfed49fcf6712cf76f26a343da7db4c28c47db0
Lorne Stalker <lorne.stalker@gmail.com>
2021-10-02 11:48:20 +0100

    adding first test

--- api/tests/app.test.js
+++ api/tests/app.test.js
@@ -1,9 +1,9 @@
 const request = require('supertest');
 const app = require('../app');
 
-describe("Test the root path", () => {
-  test("It should response the GET method", async () => {
-    const response = await request(app).get("/");
+describe("Test the projects api", () => {
+  test("GET method should respond successfully", async () => {
+    const response = await request(app).get("/projects");
     expect(response.statusCode).toBe(200);
   });
 });
