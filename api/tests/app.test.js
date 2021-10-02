47e80c46062a712cc5cf677caccee06416fb46ca
Lorne Stalker <lorne.stalker@gmail.com>
2021-10-02 12:08:27 +0100

    adding test

--- api/tests/app.test.js
+++ api/tests/app.test.js
@@ -2,8 +2,13 @@ const request = require('supertest');
 const app = require('../app');
 
 describe("Test the projects api", () => {
-  test("GET method should respond successfully", async () => {
+  test("GET all method should respond successfully", async () => {
     const response = await request(app).get("/projects");
     expect(response.statusCode).toBe(200);
+  });
+  
+  test("GET single project by ID method should return Not Found", async () => {
+    const response = await request(app).get("/projects/1");
+    expect(response.statusCode).toBe(404);
   });
 });
