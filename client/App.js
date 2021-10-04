21af0539b7690ebe52fd66269de5f38ca5179791
Andrew Sharkey <sharkand@hotmail.com>
2021-10-04 09:50:44 +0100

    Installed dotenv package and inline-dotenv plugin, added later to babel.config, added .env to .gitignore

--- client/App.js
+++ client/App.js
@@ -2,10 +2,13 @@ import React, {useState, useEffect} from 'react';
 import {Text, SafeAreaView} from 'react-native';
 import Theme from './styles/Theme';
 import ProjectList from './screens/ProjectList';
-
+import 'dotenv/config';
 
 export default function App() {
   
+  const [data, setData] = useState(null);
+  const url = process.env.BASE_URL + '/projects';
+
   useEffect( () => {
     fetch(url)
     .then(results => results.json())
