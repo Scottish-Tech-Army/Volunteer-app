7c513699d0883e48c794037fec1140173eecaaa9
Keith Douglas <keith@keithdouglas.co.uk>
2022-01-13 12:09:17 +0000

    change projects to events in fetchAll

--- app/src/Services/modules/events/fetchAll.ts
+++ app/src/Services/modules/events/fetchAll.ts
@@ -5,5 +5,5 @@ import { Events } from '.'
 export default (build: EndpointBuilder<any, any, any>) =>
   build.query<Events, string>({
     query: () => `${Config.STA_BASE_URL}/airtable/events`,
-    transformResponse: data => data?.projects,
+    transformResponse: data => data?.events,
   })
