0a2d7b8a7c4d5f97bdc38f443b6ec7eaf800900c
Catriona MacLellan <catriona.maclellan@sky.uk>
2022-01-21 15:30:10 +0000

    Updates Project.js with axios changes

--- api/routes/projects.js
+++ api/routes/projects.js
@@ -8,54 +8,60 @@ const app = express();
 const api_key = process.env.API_KEY;
 const email = process.env.EMAIL;
 
-router.get('/', (req, res, next) => {
-    const fetchAll = async(u) => {
-       axios(u, {
-          method: 'GET',
-          headers: {
-            'Authorization': `Basic ${Buffer.from(
-              // below use email address you used for jira and generate token from jira
-              `${email}:${api_key}`
-            ).toString('base64')}`,
-            'Accept': 'application/json'
-          }
-        })    
-        .then(json => {
-        const dataArray = []
-          if (json.data.issues[0]['key'].search('RES') > -1) {
-           
-            const ResData = json.data.issues.map(x => (
-            dataArray.push( {
-                res_id: x['id'],
-                  jobRole: x['fields'].customfield_10113,
-                  projectType: x['fields'].customfield_10112,
-                  suitableForBuddy: x['fields'].customfield_10108 ? x['fields'].customfield_10108.value : 'none',
-                  candidateTime: x['fields'].customfield_10062 ? x['fields'].customfield_10062 : 'none',
-                  candidateCoreSkills: x['fields'].customfield_10061 ? x['fields'].customfield_10061 : 'none'    
-              })));
-              
-          } else if(json.data.issues[0]['key'].search('IT') > -1){
 
-            const ItData = json.data.issues.map(x => (
-            dataArray.push( { 
-                it_id: x['id'],
-                projectName: x['fields'].summary, 
-                charityName: x['fields'].customfield_10027,  
-                charityVideo: x['fields'].customfield_10159 ? x['fields'].customfield_10159 : 'none'
-              })));
-          }
-          return res.status(200).send(dataArray);
-    
-        })
-        .catch(err => next(err));
+// axios.defaults.baseURL= 'https://sta2020.atlassian.net/rest/api/2/search?jql=project='
 
-      } 
-
-const path = ['https://sta2020.atlassian.net/rest/api/2/search?jql=project=RES&startAt=101&maxResults=1000',
-            'https://sta2020.atlassian.net/rest/api/2/search?jql=project=IT&startAt=101&maxResults=1000'];
-
-path.forEach(fetchAll);
-
-});
+router.get('/', async (req, res, next) =>  {
+  const ResArray = []
+  const ITArray = []
+  const ResGet = await axios('https://sta2020.atlassian.net/rest/api/2/search?jql=project=RES&startAt=101&maxResults=1000', {
+    method: 'GET',
+    headers: {
+      'Authorization': `Basic ${Buffer.from(
+        // below use email address you used for jira and generate token from jira
+        `${email}:${api_key}`
+      ).toString('base64')}`,
+      'Accept': 'application/json'
+    }
+  })
+  .then (json => {
+    const ResData = json.data.issues.map(x => (
+      ResArray.push( {
+          res_id: x['id'],
+            jobRole: x['fields'].customfield_10113,
+            projectType: x['fields'].customfield_10112,
+            suitableForBuddy: x['fields'].customfield_10108 ? x['fields'].customfield_10108.value : 'none',
+            candidateTime: x['fields'].customfield_10062 ? x['fields'].customfield_10062 : 'none',
+            candidateCoreSkills: x['fields'].customfield_10061 ? x['fields'].customfield_10061 : 'none'    
+        })));
+        res.status(200)
+   })
+ .then (data => {
+  axios('https://sta2020.atlassian.net/rest/api/2/search?jql=project=IT&startAt=101&maxResults=1000', {
+    method: 'GET',
+    headers: {
+      'Authorization': `Basic ${Buffer.from(
+        // below use email address you used for jira and generate token from jira
+        `${email}:${api_key}`
+      ).toString('base64')}`,
+      'Accept': 'application/json'
+    }
+  })
+  .then(json => {
+    const ItData = json.data.issues.map(x => (
+      ITArray.push( { 
+          it_id: x['id'],
+          projectName: x['fields'].summary, 
+          charityName: x['fields'].customfield_10027,  
+          charityVideo: x['fields'].customfield_10159 ? x['fields'].customfield_10159 : 'none'
+        })));
+        
+ })
+ .then(() => {
+  let final = {'res': ResArray, 'it' : ITArray}
+   res.status(200).send(final)
+ })
+ }) 
+})
 
 module.exports = router;
