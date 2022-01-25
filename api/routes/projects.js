8c6bf607aa2564ea95b358faf17915c9c0ad454b
hamid <hamidq786@hotmail.com>
2022-01-25 23:53:20 +0000

    update api projets.js file to return all jira data

--- api/routes/projects.js
+++ api/routes/projects.js
@@ -8,60 +8,99 @@ const app = express();
 const api_key = process.env.API_KEY;
 const email = process.env.EMAIL;
 
-
 // axios.defaults.baseURL= 'https://sta2020.atlassian.net/rest/api/2/search?jql=project='
-
 router.get('/', async (req, res, next) =>  {
-  const ResArray = []
-  const ITArray = []
-  const ResGet = await axios('https://sta2020.atlassian.net/rest/api/2/search?jql=project=RES&startAt=101&maxResults=1000', {
-    method: 'GET',
-    headers: {
-      'Authorization': `Basic ${Buffer.from(
-        // below use email address you used for jira and generate token from jira
-        `${email}:${api_key}`
-      ).toString('base64')}`,
-      'Accept': 'application/json'
-    }
-  })
-  .then (json => {
-    const ResData = json.data.issues.map(x => (
-      ResArray.push( {
-          res_id: x['id'],
-            jobRole: x['fields'].customfield_10113,
-            projectType: x['fields'].customfield_10112,
-            suitableForBuddy: x['fields'].customfield_10108 ? x['fields'].customfield_10108.value : 'none',
-            candidateTime: x['fields'].customfield_10062 ? x['fields'].customfield_10062 : 'none',
-            candidateCoreSkills: x['fields'].customfield_10061 ? x['fields'].customfield_10061 : 'none'    
-        })));
-        res.status(200)
-   })
- .then (data => {
-  axios('https://sta2020.atlassian.net/rest/api/2/search?jql=project=IT&startAt=101&maxResults=1000', {
-    method: 'GET',
-    headers: {
-      'Authorization': `Basic ${Buffer.from(
-        // below use email address you used for jira and generate token from jira
-        `${email}:${api_key}`
-      ).toString('base64')}`,
-      'Accept': 'application/json'
-    }
-  })
-  .then(json => {
-    const ItData = json.data.issues.map(x => (
-      ITArray.push( { 
+  const ResArray = [];
+  const ItArray = [];
+  
+  console.log('api_key', api_key);
+  console.log('Email', email);
+
+  const callAllResData = Promise.resolve(jiraResourceDataCall(0));
+  const callAllItData = Promise.resolve(jiraItDataCall(0));
+   
+   async function jiraResourceDataCall (startAt) { 
+    const jiraRes = await axios.get(`https://sta2020.atlassian.net/rest/api/2/search?jql=project=RES&startAt=${startAt}&maxResults=1000`, {
+        headers: {
+          'Authorization': `Basic ${Buffer.from(
+            // below use email address you used for jira and generate token from jira
+            `${email}:${api_key}`
+          ).toString('base64')}`,
+          'Accept': 'application/json'
+        }
+      })
+        console.log(`startAt: ${jiraRes.data.startAt}`);
+        console.log(`maxResults: ${jiraRes.data.maxResults}`);
+        console.log(`total: ${jiraRes.data.total}`);
+
+        let ResTotalData = parseInt(jiraRes.data.total);
+
+        const ResourceDataDump = jiraRes.data.issues.map(x => (
+          ResArray.push({
+              res_id: x['id'],
+                jobRole: x['fields'].customfield_10113,
+                projectType: x['fields'].customfield_10112,
+                suitableForBuddy: x['fields'].customfield_10108 ? x['fields'].customfield_10108.value : 'none',
+                candidateTime: x['fields'].customfield_10062 ? x['fields'].customfield_10062 : 'none',
+                candidateCoreSkills: x['fields'].customfield_10061 ? x['fields'].customfield_10061 : 'none'    
+          })
+        ));
+        if(ResArray.length < ResTotalData){
+          let ResStartResultSearch = ResArray.length;
+          console.log(startAt);
+          console.log(ResArray.length);
+          console.log(ResTotalData);
+          return jiraResourceDataCall(ResStartResultSearch);
+        } 
+        return;
+  }
+
+  async function jiraItDataCall (ItstartAt) {
+    const jiraIt = await axios.get(`https://sta2020.atlassian.net/rest/api/2/search?jql=project=IT&startAt=${ItstartAt}&maxResults=1000`, {
+      headers: {
+        'Authorization': `Basic ${Buffer.from(
+          // below use email address you used for jira and generate token from jira
+          `${email}:${api_key}`
+        ).toString('base64')}`,
+        'Accept': 'application/json'
+      }
+    })
+    console.log(`startAt: ${jiraIt.data.startAt}`);
+    console.log(`maxResults: ${jiraIt.data.maxResults}`);
+    console.log(`total: ${jiraIt.data.total}`);
+
+    let ItTotalData = parseInt(jiraIt.data.total);
+
+    const ItData = jiraIt.data.issues.map(x => (
+      ItArray.push({ 
           it_id: x['id'],
           projectName: x['fields'].summary, 
           charityName: x['fields'].customfield_10027,  
           charityVideo: x['fields'].customfield_10159 ? x['fields'].customfield_10159 : 'none'
-        })));
-        
- })
- .then(() => {
-  let final = {'res': ResArray, 'it' : ITArray}
-   res.status(200).send(final)
- })
- }) 
+        })
+    ));
+    if(ItArray.length < ItTotalData){
+      let ItStartResultSearch = ItArray.length;
+      console.log(ItstartAt);
+      console.log(ItArray.length);
+      console.log(ItTotalData);
+      return jiraItDataCall(ItStartResultSearch);
+    } 
+    return;
+  }
+  Promise.all([callAllResData, callAllItData]).then(() => {
+    const final = {
+      'jiraResBoard': {
+        'number of results': ResArray.length, 
+        'data': ResArray
+        },
+      'jiraItBoard':  {
+        'number of results': ItArray.length, 
+        'data': ItArray
+        }
+    };
+    res.status(200).send(final);
+  })
 })
 
 module.exports = router;
