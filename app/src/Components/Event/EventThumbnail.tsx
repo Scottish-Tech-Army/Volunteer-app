127470ea88e7d34de49d9f66252e9646454113e5
Keith Douglas <keith@keithdouglas.co.uk>
2022-02-01 16:09:34 +0000

    working Event image

--- app/src/Components/Event/EventThumbnail.tsx
+++ app/src/Components/Event/EventThumbnail.tsx
@@ -1,25 +1,20 @@
 import React, { FC }  from 'react'
-import { Image } from 'react-native'
 import styled from 'styled-components/native'
 
 interface EventThumbnailProps {
     thumbnailUri: string
 }
 
-const ThumbnailView = styled.Text`
-    width: 48px;
-    height: 48px;
+const ThumbnailImage = styled.Image`
+    width: 96px;
+    height: 96px;
     border-radius: 5px;
     margin-right: 10px;
 `
 
 const EventThumbnail: FC<EventThumbnailProps> = ({thumbnailUri}) => {
   return (
-    <ThumbnailView>
-        <Image source={{
-          uri: thumbnailUri,
-        }}/>
-    </ThumbnailView>
+    <ThumbnailImage source={{ uri: thumbnailUri }}/>
   )
 }
 
