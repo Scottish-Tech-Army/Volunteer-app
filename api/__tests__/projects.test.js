c11e29d7ba80cca4f44606c7d315df7d73c3e073
Joe Derry Hall <joe@joederryhall.com>
2022-02-06 15:42:37 +0000

    Fix projects test timing out

--- api/__tests__/projects.test.js
+++ api/__tests__/projects.test.js
@@ -1,14 +1,14 @@
 const request = require('supertest');
 const app = require('../app');
 
-describe("Test the projects api", () => {
-  test("GET all method should respond successfully", async () => {
-    const response = await request(app).get("/projects");
+describe('Test the projects api', () => {
+  test('GET all method should respond successfully', async () => {
+    const response = await request(app).get('/projects');
     expect(response.statusCode).toBe(200);
-  });
-  
-  test("GET single project by ID method should return Not Found", async () => {
-    const response = await request(app).get("/projects/1");
+  }, 25000);
+
+  test('GET single project by ID method should return Not Found', async () => {
+    const response = await request(app).get('/projects/1');
     expect(response.statusCode).toBe(404);
   });
 });
