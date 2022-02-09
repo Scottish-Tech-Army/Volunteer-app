5f19624f745bf199e5461a38ec8ad8a9d4aef747
Catriona MacLellan <catriona.maclellan@sky.uk>
2022-02-09 20:33:28 +0000

    Added function to link data from the two boards

--- api/routes/projects.js
+++ api/routes/projects.js
@@ -9,45 +9,46 @@ const api_key = process.env.API_KEY;
 const email = process.env.EMAIL;
 
 // axios.defaults.baseURL= 'https://sta2020.atlassian.net/rest/api/2/search?jql=project='
-router.get('/', async (req, res, next) =>  {
+router.get('/', async (req, res, next) => {
   const ResArray = [];
   const ItArray = [];
 
   const callAllResData = Promise.resolve(jiraResourceDataCall(0));
   const callAllItData = Promise.resolve(jiraItDataCall(0));
-   
-   async function jiraResourceDataCall (startAt) { 
+
+  async function jiraResourceDataCall(startAt) {
     const jiraRes = await axios.get(`https://sta2020.atlassian.net/rest/api/2/search?jql=project=RES&startAt=${startAt}&maxResults=1000`, {
-        headers: {
-          'Authorization': `Basic ${Buffer.from(
-            // below use email address you used for jira and generate token from jira
-            `${email}:${api_key}`
-          ).toString('base64')}`,
-          'Accept': 'application/json'
-        }
+      headers: {
+        'Authorization': `Basic ${Buffer.from(
+          // below use email address you used for jira and generate token from jira
+          `${email}:${api_key}`
+        ).toString('base64')}`,
+        'Accept': 'application/json'
+      }
+    })
+
+    let ResTotalData = parseInt(jiraRes.data.total);
+
+    const ResourceDataDump = jiraRes.data.issues.map(x => (
+      ResArray.push({
+        res_id: x['id'],
+        it_related_field_id: x['fields'].customfield_10109,
+        jobRole: x['fields'].customfield_10113,
+        projectType: x['fields'].customfield_10112,
+        suitableForBuddy: x['fields'].customfield_10108 ? x['fields'].customfield_10108.value : 'none',
+        candidateTime: x['fields'].customfield_10062 ? x['fields'].customfield_10062 : 'none',
+        candidateCoreSkills: x['fields'].customfield_10061 ? x['fields'].customfield_10061 : 'none'
       })
+    ));
 
-        let ResTotalData = parseInt(jiraRes.data.total);
-
-        const ResourceDataDump = jiraRes.data.issues.map(x => (
-          ResArray.push({
-              res_id: x['id'],
-                jobRole: x['fields'].customfield_10113,
-                projectType: x['fields'].customfield_10112,
-                suitableForBuddy: x['fields'].customfield_10108 ? x['fields'].customfield_10108.value : 'none',
-                candidateTime: x['fields'].customfield_10062 ? x['fields'].customfield_10062 : 'none',
-                candidateCoreSkills: x['fields'].customfield_10061 ? x['fields'].customfield_10061 : 'none'    
-          })
-        ));
-
-        if(ResArray.length < ResTotalData){
-          let ResStartResultSearch = ResArray.length;
-          return jiraResourceDataCall(ResStartResultSearch);
-        } 
-        return;
+    if (ResArray.length < ResTotalData) {
+      let ResStartResultSearch = ResArray.length;
+      return jiraResourceDataCall(ResStartResultSearch);
+    }
+    return;
   }
 
-  async function jiraItDataCall (ItstartAt) {
+  async function jiraItDataCall(ItstartAt) {
     const jiraIt = await axios.get(`https://sta2020.atlassian.net/rest/api/2/search?jql=project=IT&startAt=${ItstartAt}&maxResults=1000`, {
       headers: {
         'Authorization': `Basic ${Buffer.from(
@@ -57,35 +58,57 @@ router.get('/', async (req, res, next) =>  {
         'Accept': 'application/json'
       }
     })
- 
+
     let ItTotalData = parseInt(jiraIt.data.total);
 
     const ItData = jiraIt.data.issues.map(x => (
-      ItArray.push({ 
-          it_id: x['id'],
-          projectName: x['fields'].summary, 
-          charityName: x['fields'].customfield_10027,  
-          charityVideo: x['fields'].customfield_10159 ? x['fields'].customfield_10159 : 'none'
-        })
+      ItArray.push({
+        it_key: x['key'],
+        projectName: x['fields'].summary,
+        charityName: x['fields'].customfield_10027,
+        charityVideo: x['fields'].customfield_10159 ? x['fields'].customfield_10159 : 'none'
+      })
     ));
-    if(ItArray.length < ItTotalData){
+    if (ItArray.length < ItTotalData) {
       let ItStartResultSearch = ItArray.length;
       return jiraItDataCall(ItStartResultSearch);
-    } 
+    }
     return;
   }
+
+  function linkData(ResArray, ItArray) {
+    const FinalArray = [];
+    for (var i = 0; i < ResArray.length; i++) {
+      for (var j = 0; j < ItArray.length; j++) {
+        for (var ResData in ResArray[i]) {
+          for (var ItData in ItArray[j]) {
+            if (ResArray[i][ResData] == ItArray[j][ItData]) {
+              FinalArray.push({
+                ResData: ResArray[i],
+                ItData: ItArray[j]
+              })
+            }
+            break;
+          }
+        }
+      }
+    }
+    console.log(FinalArray);
+    res.status(200).send(FinalArray);
+  }
+
   Promise.all([callAllResData, callAllItData]).then(() => {
     const final = {
       'jiraResBoard': {
-        'number of results': ResArray.length, 
+        'number of results': ResArray.length,
         'data': ResArray
-        },
-      'jiraItBoard':  {
-        'number of results': ItArray.length, 
+      },
+      'jiraItBoard': {
+        'number of results': ItArray.length,
         'data': ItArray
-        }
+      }
     };
-    res.status(200).send(final);
+    return linkData(ResArray, ItArray);
   })
 })
 
