7a88f7024e3aa2c019cc285f4680e4d9e60d6f96
steven <steven@burrows.plus.com>
2022-02-15 14:56:28 +0000

    added the tests for the single project

--- api/__tests__/projects.test.js
+++ api/__tests__/projects.test.js
@@ -78,4 +78,73 @@ describe('Test the projects api', () => {
     const response = await request(app).get('/projects/1');
     expect(response.statusCode).toBe(404);
   });
+
+  // set up data for single project
+
+  const singleProjectsMock = nock('http://localhost:3000')
+  .get('/projects/single?res=15486&it=IT-347')
+  .reply(200, {
+      "res_id": "15486",
+      "it_related_field_id": "IT-347",
+      "jobRole": "Web Developer",
+      "projectType": "Design and Build",
+      "suitableForBuddy": "Yes",
+      "candidateTime": "10 Days",
+      "candidateCoreSkills": {
+        "content": [
+          {
+            "type": "paragraph",
+            "content": [
+              {
+                "type": "text",
+                "text": "Web Database Design and Build, "
+              }
+            ]
+          },
+        ],
+        "type": "doc",
+        "version": 1
+      },
+      "it_key": "IT-347",
+      "projectSummary": [
+        {
+          "type": "paragraph",
+          "content": [
+            {
+              "type": "text",
+              "text": "I have got an idea that would help unpaid carers to find professional help with their caring situation. It needs a bit of tech-ing though."
+            }
+          ]
+        },
+        {
+          "type": "paragraph",
+          "content": [
+            {
+              "type": "text",
+              "text": "Julie."
+            }
+          ]
+        }
+      ],
+      "projectName": "Carers of East Lothian Website Help",
+      "charityName": "Carers of East Lothian",
+      "charityVideo": "none"
+    
+  });
+
+  // Run tests
+
+  describe('Test the projects api', () => {
+    test('GET all method should respond successfully', async () => {
+      const response = await axios.get('http://localhost:3000/projects/single?res=15486&it=IT-347');
+  
+      singleProjectsMock.done();
+  
+      expect(response.status).toBe(200);
+  
+      expect(response.data.res_id).toBe('15486');
+      expect(response.data.it_key).toBe('IT-347');
+  
+    });
+  })
 });
