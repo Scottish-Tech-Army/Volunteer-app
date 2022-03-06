2b4590140117e5891c6bfda8add48aa88a629309
PammyLo <pamela@tuminds.com>
2022-03-06 17:24:17 +0000

    Add onclick navigation to home search icon

--- app/src/Components/Project/ProjectSearch.tsx
+++ app/src/Components/Project/ProjectSearch.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import FontAwesome from 'react-native-vector-icons/FontAwesome'
 import styled from 'styled-components/native'
+import { navigate } from '@/Navigators/utils'
 
-const SearchView = styled.View`
+const SearchView = styled.TouchableOpacity`
   display: flex;
   flex-direction: row;
   justify-content: flex-end;
@@ -11,7 +12,7 @@ const SearchView = styled.View`
 
 const ProjectSearch = () => {
   return (
-    <SearchView>
+    <SearchView onPress={() => { navigate('Search', '') }}>
       <FontAwesome name="search" size={38} />
     </SearchView>
   )
