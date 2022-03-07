06f4bef3c8d676c1901465947df9bc50ac436813
Joe Derry Hall <joe@joederryhall.com>
2022-03-07 14:15:19 +0000

    Update single project route test

--- api/__tests__/routes/projects.test.js
+++ api/__tests__/routes/projects.test.js
@@ -34,70 +34,26 @@ describe('Test the projects api', () => {
     expect(response.statusCode).toBe(404);
   });
 
-  // set up data for single project
-
-  const singleProjectsMock = nock('http://localhost:3000')
-    .get('/projects/single?res=15486&it=IT-347')
-    .reply(200, {
-      res_id: '15486',
-      it_related_field_id: 'IT-347',
-      jobRole: 'Web Developer',
-      projectType: 'Design and Build',
-      suitableForBuddy: 'Yes',
-      candidateTime: '10 Days',
-      candidateCoreSkills: {
-        content: [
-          {
-            type: 'paragraph',
-            content: [
-              {
-                type: 'text',
-                text: 'Web Database Design and Build, ',
-              },
-            ],
-          },
-        ],
-        type: 'doc',
-        version: 1,
-      },
-      it_key: 'IT-347',
-      projectSummary: [
-        {
-          type: 'paragraph',
-          content: [
-            {
-              type: 'text',
-              text: 'I have got an idea that would help unpaid carers to find professional help with their caring situation. It needs a bit of tech-ing though.',
-            },
-          ],
-        },
-        {
-          type: 'paragraph',
-          content: [
-            {
-              type: 'text',
-              text: 'Julie.',
-            },
-          ],
-        },
-      ],
-      projectName: 'Carers of East Lothian Website Help',
-      charityName: 'Carers of East Lothian',
-      charityVideo: 'none',
-    });
-
-  // Run tests
-
   describe('Test the projects api', () => {
     test('GET a single project method should respond successfully', async () => {
-      const response = await axios.get('http://localhost:3000/projects/single?res=15486&it=IT-347');
+      // Set up fake test data
+      const fakeProjectResource = projectsTestData.fakeProjectResourceObject();
+
+      // Mock dependencies
+      const singleProjectsMock = nock('http://localhost:3000')
+        .get(`/projects/single?res=${fakeProjectResource.res_id}&it=${fakeProjectResource.it_key}`)
+        .reply(200, fakeProjectResource);
+
+      // Run test
+      const response = await axios.get(
+        `http://localhost:3000/projects/single?res=${fakeProjectResource.res_id}&it=${fakeProjectResource.it_key}`,
+      );
 
       singleProjectsMock.done();
 
       expect(response.status).toBe(200);
-
-      expect(response.data.res_id).toBe('15486');
-      expect(response.data.it_key).toBe('IT-347');
+      expect(response.data.res_id).toBe(fakeProjectResource.res_id);
+      expect(response.data.it_key).toBe(fakeProjectResource.it_key);
     });
   });
 });
