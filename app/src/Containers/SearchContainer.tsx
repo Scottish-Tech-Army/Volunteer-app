bfa98e03f688a51be0a7baad7a98142c41cb40be
PammyLo <pamela@tuminds.com>
2022-03-16 14:48:54 +0000

    show all quick search option in search screen

--- app/src/Containers/SearchContainer.tsx
+++ app/src/Containers/SearchContainer.tsx
@@ -1,41 +1,61 @@
-import React from 'react'
+import React, { FC } from 'react'
 import styled from 'styled-components/native'
 import { FlatList, ScrollView, SafeAreaView, SectionList } from 'react-native'
 import TopOfApp from '@/Components/TopOfApp'
 
-const roles =
-    [
-        "Web Developer",
-        "Tech Support",
-        "UI/UX",
-        "Data Analyst",
-        "Researcher",
-        "Scrum Master",
-        "BA/PM",
-        "Test Analyst",
-    ]
+const Roles = [
+    "Web Developer",
+    "Tech Support",
+    "UI/UX",
+    "Researcher",
+    "Scrum Master",
+    "BA/PM",
+]
+ 
+const Causes = [
+    "Health & Social Care",
+    "Education & Youth",
+    "Arts & Culture",
+    "Environment & Conservation",
+    "Community Projects",
+    "Internal STA"
+]
+const TechStack = [
+    "Java",
+    "JavaScript",
+    "Python",
+    "AWS",
+    "React Native",
+    "React"
+]
 
 const Heading = styled.Text`
   font-weight: bold;
   font-size: 18px;
-  margin: 21px 15px 0px 15px;
+  margin: 15px 15px 0px 15px;
 `
 
 const SubHeading = styled.Text`
   font-weight: bold;
-  font-size: 18px;
-  margin: 20px 15px 0px 15px;
+  font-size: 16px;
+  margin: 10px 15px 0px 15px;
+`
+
+const SectionView = styled.View`
+  display: flex;
+  flex-direction: column;
+  flex-wrap: wrap;
+  height: 180px;
 `
 
 const QuickSearchButton = styled.TouchableOpacity`
-  width: 100px;
-  height: 60px;
+  width: 28%;
+  height: 50px;
   margin: 20px 0px 0px 15px;
   padding: 5px;
   background-color: #e3e3e3;
   border: ${props => `1px solid ${props.theme.colors.staBlack}`};
   display: flex;
-  flex-direction: column;
   justify-content: center;
 `
 
@@ -46,25 +66,36 @@ const QuickSearchTitle = styled.Text`
 
 const SearchContainer = () => {
     return (
-        <>
-            <TopOfApp />
-            <Heading>Popular Searches</Heading>
-            <SubHeading>Roles</SubHeading>
-            <FlatList
-                horizontal
-                data={roles}
-                keyExtractor={index => index.toString()}
-                renderItem={({ item }) => {
-                    return (                       
-                            <QuickSearchButton>
-                                <QuickSearchTitle>
-                                    {item}
-                                </QuickSearchTitle>
-                            </QuickSearchButton>
-                    )
-                }}
-            />
-        </>
+        <SafeAreaView>
+            <ScrollView>
+                <TopOfApp />
+                <Heading>Popular Searches</Heading>
+                <SubHeading>Roles</SubHeading>
+                <SectionView>
+                    {Roles.map(role => (
+                        <QuickSearchButton>
+                            <QuickSearchTitle>{role}</QuickSearchTitle>
+                        </QuickSearchButton>
+                    ))}
+                </SectionView>
+                <SubHeading>Causes</SubHeading>
+                <SectionView>
+                    {Causes.map(cause => (
+                        <QuickSearchButton>
+                            <QuickSearchTitle>{cause}</QuickSearchTitle>
+                        </QuickSearchButton>
+                    ))}
+                </SectionView>
+                <SubHeading>Tech Stack / Languages</SubHeading>
+                <SectionView>
+                    {TechStack.map(tech => (
+                        <QuickSearchButton>
+                            <QuickSearchTitle>{tech}</QuickSearchTitle>
+                        </QuickSearchButton>
+                    ))}
+                </SectionView>
+            </ScrollView>
+        </SafeAreaView>
     )
 }
 
