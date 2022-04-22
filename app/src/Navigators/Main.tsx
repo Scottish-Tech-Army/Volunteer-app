1ef6fd422abd2aea7e958dd4523a873718812e26
PammyLo <pamela@tuminds.com>
2022-04-22 15:07:37 +0100

    add search result screen to nav tab menu for dev purposes - to be removed again later

--- app/src/Navigators/Main.tsx
+++ app/src/Navigators/Main.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
-import { ExampleContainer } from '@/Containers'
-import { HomeContainer } from '@/Containers'
+import { HomeContainer, ExampleContainer, ProjectSearchResultsContainer } from '@/Containers'
 import Entypo from 'react-native-vector-icons/Entypo'
 import { useTheme } from '@/Hooks'
 
@@ -22,6 +21,19 @@ const MainNavigator = () => {
           ),
         }}
       />
+
+      {/* temporary tab to be able to see search result page during dev */}
+      <Tab.Screen
+        name="SearchResult"
+        component={ProjectSearchResultsContainer}
+        options={{
+          headerShown: false,
+          tabBarIcon: () => (
+            <Entypo color={Colors.text} name="home" size={24} />
+          ),
+        }}
+      />
+
       <Tab.Screen
         name="Example"
         component={ExampleContainer}
