2ffe8b6eaed074b75e7ea109e0b6965d5dbede07
PammyLo <pamela@tuminds.com>
2022-04-22 15:06:01 +0100

    Create search results screen (container)

---
+++ app/src/Containers/ProjectSearchResultsContainer.tsx
@@ -0,0 +1,59 @@
+import React, { FC, useEffect } from 'react'
+import styled from 'styled-components/native'
+import TopOfApp from '@/Components/TopOfApp'
+import { Projects, useLazyFetchAllQuery } from '@/Services/modules/projects'
+import ProjectReturnedList from '@/Components/Project/ProjectReturnedList'
+import ProjectFilterSort from '@/Components/Project/ProjectFilterSort'
+import { Text, SafeAreaView } from 'react-native'
+
+/*
+interface SearchResultsProps {
+    searchResults: Projects
+}
+
+
+
+const ProjectSearchResultsContainer: FC<SearchResultsProps> = ({ searchResults }) => {
+    if (searchResults = [] ) return null;
+    return (
+        <SafeArea>
+            <TopOfApp />
+            <ProjectFilterSort />
+            <ProjectReturnedList data={searchResults} />
+        </SafeArea>
+    )
+}
+*/
+
+const SafeArea = styled.SafeAreaView`
+  background: ${props => props.theme.colors.appBackground};
+  color: ${props => props.theme.colors.staBlack};
+  flex: 1;
+`
+
+const ProjectSearchResultsContainer = () => {
+    const [fetchAll, { data: projects }] = useLazyFetchAllQuery()
+
+    useEffect(() => {
+        fetchAll('')
+    }, [fetchAll])
+
+    if(projects) {
+        return (
+            <SafeArea>
+                <TopOfApp />
+                <ProjectFilterSort />
+                <ProjectReturnedList data={projects} />
+            </SafeArea>
+        )
+  } else {
+    return (
+        <SafeAreaView>
+            <Text>Loading...</Text>
+        </SafeAreaView>
+    )
+}
+}
+
+
+export default ProjectSearchResultsContainer
