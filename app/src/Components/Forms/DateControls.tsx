84afd63e9d96b3392d7ce9f4d572a2c1918d6889
Dougie Richardson <dougie.richardson@outlook.com>
2022-05-22 17:20:03 +0100

    Added styled view

--- app/src/Components/Forms/DateControls.tsx
+++ app/src/Components/Forms/DateControls.tsx
@@ -1,23 +1,29 @@
 import { Calendar, CalendarList, Agenda } from 'react-native-calendars'
 import React, { FC } from 'react'
-
 import styled from 'styled-components/native'
 
 interface CalendarProps {
     markedColour: string;
 }
 
-const DateControls: FC<CalendarProps> = ({markedColour}: CalendarProps) => {
+const CalendarView = styled.View`
+    display: flex;
+    flex-direction: column;
+`
+
+const DateControls: FC<CalendarProps> = ({ markedColour }: CalendarProps) => {
     return (
-        <Calendar
-            // Collection of dates that have to be marked. Default = {}
-            markedDates={{
-                '2022-05-16': { selected: true, marked: true, selectedColor: 'blue' },
-                '2022-05-17': { marked: true },
-                '2022-05-18': { marked: true, dotColor: markedColour, activeOpacity: 0 },
-                '2022-05-19': { disabled: true, disableTouchEvent: true }
-            }}
-        />
+        <CalendarView>
+            <Calendar
+                // Collection of dates that have to be marked. Default = {}
+                markedDates={{
+                    '2022-05-16': { selected: true, marked: true, selectedColor: 'blue' },
+                    '2022-05-17': { marked: true },
+                    '2022-05-18': { marked: true, dotColor: markedColour, activeOpacity: 0 },
+                    '2022-05-19': { disabled: true, disableTouchEvent: true }
+                }}
+            />
+        </CalendarView>
     )
 }
 
