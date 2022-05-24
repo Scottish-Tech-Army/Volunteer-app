5af201c49e51b0a400e4e34907e2397fdf2376f5
Dougie Richardson <dougie.richardson@outlook.com>
2022-05-24 18:15:33 +0100

    Created events options control

---
+++ app/src/Components/Event/EventOptions.tsx
@@ -0,0 +1,37 @@
+/*
+ *  Shamelessly duplicated from ProjectOptions.tsx
+ */
+import React from 'react'
+import styled from 'styled-components/native'
+import underDevelopmentAlert from '../../Utils/UnderDevelopmentAlert'
+
+const EventOptionsView = styled.View`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  margin: 23px 10px;
+`
+
+const EventOptionsText = styled.Text`
+  font-size: 18px;
+`
+
+const EventOptionsTouch = styled.TouchableOpacity``
+
+const EventOptions = () => {
+  return (
+    <EventOptionsView>
+      <EventOptionsTouch onPress={underDevelopmentAlert}>
+        <EventOptionsText>Past</EventOptionsText>
+      </EventOptionsTouch>
+      <EventOptionsTouch onPress={underDevelopmentAlert}>
+        <EventOptionsText>Upcoming</EventOptionsText>
+      </EventOptionsTouch>
+      <EventOptionsTouch onPress={underDevelopmentAlert}>
+        <EventOptionsText>My Events</EventOptionsText>
+      </EventOptionsTouch>
+    </EventOptionsView>
+  )
+}
+
+export default EventOptions
