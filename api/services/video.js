6963e2c0dce9ad728097cf92756633c9df2d4ba2
Cat MacLellan <catmaclellan228@gmail.com>
2022-06-30 17:24:44 +0100

    Fixed formatting

--- api/services/video.js
+++ api/services/video.js
@@ -3,25 +3,25 @@ const axios = require('axios').default;
 
 async function getVideoFileFromVimeo(video) {
   if (video !== '') {
-        let parsed = new URL(video)
-        
-        
-        const vimeoId = parsed.pathname.split('/')[1]
+    let parsed = new URL(video)
 
-        if(vimeoId.match(/^[0-9]+$/)) {
-        const vimeoData = await axios.get(
-          `https://player.vimeo.com/video/${vimeoId}/config`,
-          {
-              Accept: 'application/json',
-          })
+    const vimeoId = parsed.pathname.split('/')[1]
 
-          const videoFile = vimeoData.data.request.files.progressive[0].url
-                  // console.log(videoFile)
-    return videoFile;
-        }
-        }
-     }
-    
+    // Some video returned were not Vimeo or had a different URL format
+    // Condition below will check if the third part of the Vimeo URL contains only digits as a temp workaround
+    if (vimeoId.match(/^[0-9]+$/)) {
+      const vimeoData = await axios.get(
+        `https://player.vimeo.com/video/${vimeoId}/config`,
+        {
+          Accept: 'application/json',
+        })
+
+      const videoFile = vimeoData.data.request.files.progressive[0].url
+      return videoFile;
+    }
+  }
+}
+
 module.exports = {
   getVideoFileFromVimeo,
 };
