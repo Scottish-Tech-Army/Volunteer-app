9c9a11571df7687c4e4fd312df289185123724f3
Joe Derry Hall <joe@joederryhall.com>
2022-07-21 15:01:55 +0100

    Get react-native-video working properly on Android, remove unneeded Android config changes

--- app/android/app/src/main/java/com/sta/MainApplication.java
+++ app/android/app/src/main/java/com/sta/MainApplication.java
@@ -4,7 +4,6 @@ import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
-import com.brentvatne.react.ReactVideoPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
@@ -28,7 +27,6 @@ public class MainApplication extends Application implements ReactApplication {
           List<ReactPackage> packages = new PackageList(this).getPackages();
           // Packages that cannot be autolinked yet can be added manually here, for example:
           // packages.add(new MyReactNativePackage());
-          packages.add(new ReactVideoPackage());
           return packages;
         }
 
