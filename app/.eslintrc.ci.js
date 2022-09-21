35514dd10d314fb1e0fd9ef5f91a53c979cbd814
rc-1988 <rosanna.cretney@gmail.com>
2022-09-21 22:03:41 +0100

    Extended ESLint config for CI checks: require a minimum level of JSDoc comment coverage

---
+++ app/.eslintrc.ci.js
@@ -0,0 +1,19 @@
+module.exports = {
+  extends: ['.eslintrc.js', 'plugin:jsdoc/recommended'],
+  rules: {
+    'jsdoc/check-alignment': ['off'], // leave this to prettier to handle
+    'jsdoc/newline-after-description': ['off'],
+    'jsdoc/require-file-overview': 'error',
+    'jsdoc/require-jsdoc': [
+      'warn',
+      {
+        publicOnly: true,
+        require: {
+          ArrowFunctionExpression: true,
+          FunctionExpression: true,
+        },
+        minLineCount: 5,
+      },
+    ],
+  },
+}
