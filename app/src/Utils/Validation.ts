299a562ff4abfa3f80908feb46c2a6c9e13f6922
rc-1988 <rosanna.cretney@gmail.com>
2022-10-12 21:28:14 +0100

    Fix the CI failure introduced (deliberately) in #c01cadb

--- app/src/Utils/Validation.ts
+++ app/src/Utils/Validation.ts
@@ -1,3 +1,12 @@
+/**
+ * @file Validation utilities
+ */
+
+/**
+ * Returns true if and only if email represents a syntactically valid email address.
+ * @param {string} email Email address to validate
+ * @returns
+ */
 export const validateEmail = (email: string): boolean =>
   Boolean(
     email.match(
