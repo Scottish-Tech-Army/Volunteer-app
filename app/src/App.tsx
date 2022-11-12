a950de5558610bdd043d0cacb313b02aea8feea1
Dougie Richardson <me@dougiewougie.com>
2022-11-12 13:07:29 +0000

    Aegon theming

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -10,25 +10,22 @@ import ApplicationNavigator from '@/Navigators/Application'
 import ThemeProvider from '@/Theme/OldTheme'
 import './Translations'
 import '@/Components/Icons/init'
-import { NativeBaseProvider } from 'native-base'
 
 const App = () => (
-  <NativeBaseProvider>
-    <Provider store={store}>
-      {/**
-       * PersistGate delays the rendering of the app's UI until the persisted state has been retrieved
-       * and saved to redux.
-       * The `loading` prop can be `null` or any react instance to show during loading (e.g. a splash screen),
-       * for example `loading={<SplashScreen />}`.
-       * @see https://github.com/rt2zz/redux-persist/blob/master/docs/PersistGate.md
-       */}
-      <PersistGate loading={null} persistor={persistor}>
-        <ThemeProvider>
-          <ApplicationNavigator />
-        </ThemeProvider>
-      </PersistGate>
-    </Provider>
-  </NativeBaseProvider>
+  <Provider store={store}>
+    {/**
+     * PersistGate delays the rendering of the app's UI until the persisted state has been retrieved
+     * and saved to redux.
+     * The `loading` prop can be `null` or any react instance to show during loading (e.g. a splash screen),
+     * for example `loading={<SplashScreen />}`.
+     * @see https://github.com/rt2zz/redux-persist/blob/master/docs/PersistGate.md
+     */}
+    <PersistGate loading={null} persistor={persistor}>
+      <ThemeProvider>
+        <ApplicationNavigator />
+      </ThemeProvider>
+    </PersistGate>
+  </Provider>
 )
 
 export default App
--- app/src/Containers/ProfileContainer.tsx
+++ app/src/Containers/ProfileContainer.tsx
@@ -13,16 +13,32 @@ import {
   Center,
   Spacer,
   ScrollView,
+  extendTheme,
+  NativeBaseProvider,
 } from 'native-base'
 import { useDispatch, useSelector } from 'react-redux'
 import { changeWelcome, WelcomeState } from '@/Store/Welcome'
-import { useTheme } from '@/Hooks'
 import { version } from '../../package.json'
 import { changeTheme, ThemeState } from '@/Store/Theme'
 import { Brand } from '@/Components'
 
 const ProfileContainer = () => {
-  const { Fonts, Gutters } = useTheme()
+  const Aegon = extendTheme({
+    components: {
+      Text: {
+        baseStyle: {
+          color: '#3C3C3B',
+          fontFamily: 'Poppins-Medium',
+        },
+      },
+      Heading: {
+        baseStyle: {
+          color: '#D1338A',
+          fontFamily: 'Poppins-Medium',
+        },
+      },
+    },
+  })
   const welcomeState = useSelector(
     (state: { welcome: WelcomeState }) => state.welcome.show,
   )
@@ -37,61 +53,69 @@ const ProfileContainer = () => {
     dispatch(changeWelcome({ welcome, show }))
   }
   return (
-    <ScrollView>
-      <VStack safeAreaTop space={4} padding={4}>
-        <Center>
-          <Brand />
-        </Center>
-        <Heading>Theme</Heading>
-        <Radio.Group
-          name="themeRadioGroup"
-          accessibilityLabel="Application theme"
-          onChange={value => {
-            if (value === 'null') {
-              onChangeTheme({ darkMode: null })
-            }
-            if (value === 'true') {
-              onChangeTheme({ darkMode: true })
-            } else {
-              onChangeTheme({ darkMode: false })
-            }
-          }}
-          defaultValue={themeState}
-        >
-          <Radio value="null" my={1}>
-            Follow system setting
-          </Radio>
-          <Radio value="true" my={1}>
-            Dark mode
-          </Radio>
-          <Radio value="false" my={1}>
-            Light mode
-          </Radio>
-        </Radio.Group>
-        <Heading>Welcome screen</Heading>
-        <Checkbox
-          value="welcome"
-          accessibilityLabel="Show a splash screen when the app starts"
-          isChecked={welcomeState}
-          onChange={() => onChangeSplash({ show: !welcomeState })}
-        >
-          Show a splash screen when the app starts
-        </Checkbox>
-        <Spacer />
-        <Center>
-          <Text>Version</Text>
-          <Text>{version}</Text>
-        </Center>
-        <HStack safeAreaBottom space="4" justifyContent={'center'}>
-          <Link href="https://www.scottishtecharmy.org/app-privacy-policy">
-            Privacy policy
-          </Link>
-          <Link href="https://www.scottishtecharmy.org/app-terms-conditions">
-            Terms and conditions
-          </Link>
-        </HStack>
-      </VStack>
-    </ScrollView>
+    // NativeBase would normally wrap the top level component but
+    // I've put it here to separate concerns while evaluating NativeBase.
+    <NativeBaseProvider theme={Aegon}>
+      <ScrollView>
+        <VStack safeAreaTop space={4} padding={4}>
+          <Center>
+            <Brand />
+          </Center>
+          <Heading>Theme</Heading>
+          <Radio.Group
+            colorScheme={'pink'}
+            name="themeRadioGroup"
+            accessibilityLabel="Application theme"
+            // This is hacky sorry. Depending on the theming system
+            // we employ this would be tidied up.
+            onChange={value => {
+              if (value === 'null') {
+                onChangeTheme({ darkMode: null })
+              }
+              if (value === 'true') {
+                onChangeTheme({ darkMode: true })
+              } else {
+                onChangeTheme({ darkMode: false })
+              }
+            }}
+            defaultValue={themeState}
+          >
+            <Radio value="null" my={1}>
+              Follow system setting
+            </Radio>
+            <Radio value="true" my={1}>
+              Dark mode
+            </Radio>
+            <Radio value="false" my={1}>
+              Light mode
+            </Radio>
+          </Radio.Group>
+          <Heading>Welcome screen</Heading>
+          <Checkbox
+            colorScheme={'pink'}
+            value="welcome"
+            accessibilityLabel="Show a splash screen when the app starts"
+            isChecked={welcomeState}
+            onChange={() => onChangeSplash({ show: !welcomeState })}
+          >
+            Show splash screen on app launch
+          </Checkbox>
+          <Spacer />
+          <Center>
+            <Text>Version</Text>
+            <Text>{version}</Text>
+          </Center>
+          <HStack safeAreaBottom space="4" justifyContent={'center'}>
+            <Link href="https://www.scottishtecharmy.org/app-privacy-policy">
+              Privacy policy
+            </Link>
+            <Link href="https://www.scottishtecharmy.org/app-terms-conditions">
+              Terms and conditions
+            </Link>
+          </HStack>
+        </VStack>
+      </ScrollView>
+    </NativeBaseProvider>
   )
 }
 
