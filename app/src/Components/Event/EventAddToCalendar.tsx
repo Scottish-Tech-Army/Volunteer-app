3a8a3c3d4e48fecb387303a39737ba244245e839
Joe Derry Hall <joe@joederryhall.com>
2022-11-15 14:37:32 +0000

    Work in progress

---
+++ app/src/Components/Event/EventAddToCalendar.tsx
@@ -0,0 +1,75 @@
+/**
+ * @file Event add to calendar button.
+ */
+
+import React, { FC, useState } from 'react'
+import { Alert, Platform } from 'react-native'
+import RNCalendarEvents, {
+  CalendarEventWritable,
+  Options,
+} from 'react-native-calendar-events' // see docs at https://github.com/wmcmahan/react-native-calendar-events
+import IconAndLabel from '@/Components/IconAndLabel'
+import { Event } from '@/Services/modules/events'
+import { end } from '@/Utils/Events'
+
+interface EventAddToCalendarProps {
+  event: Event
+}
+
+/**
+ * Component for an event add to calendar button
+ *
+ * @param {EventDetailsProps} props The component props
+ * @param {Event} props.event The event
+ * @returns ReactElement Component
+ */
+const EventAddToCalendar: FC<EventAddToCalendarProps> = ({ event }) => {
+  const [eventAdded, setEventAdded] = useState(false)
+
+  const onPress = async () => {
+    if (eventAdded) return
+
+    try {
+      let permissionToAccessCalendar = await RNCalendarEvents.checkPermissions()
+
+      if (permissionToAccessCalendar !== 'authorized') {
+        permissionToAccessCalendar = await RNCalendarEvents.requestPermissions()
+      }
+
+      if (permissionToAccessCalendar === 'authorized') {
+        const eventEnd = end(event)
+        const eventDetails = {
+          startDate: `${event.date}T${event.time}:00.000Z`,
+          endDate: `${eventEnd.date}T${eventEnd.time}:00.000Z`,
+          description: event.description,
+        } as CalendarEventWritable
+
+        // Fix calendar syncing issue specific to Android
+        const eventOptions =
+          Platform.OS === 'android' ? ({ sync: true } as Options) : undefined
+
+        const deviceEventId = await RNCalendarEvents.saveEvent(
+          event.name,
+          eventDetails,
+          eventOptions,
+        )
+
+        if (deviceEventId) setEventAdded(true)
+      } else {
+        Alert.alert('Sorry, we ran into a problem adding this to your calendar')
+      }
+    } catch (error) {
+      console.error('Error accessing device calendar', error)
+    }
+  }
+
+  return (
+    <IconAndLabel
+      icon={eventAdded ? 'check-square' : 'calendar'}
+      onPress={onPress}
+      text="Add to calendar"
+    />
+  )
+}
+
+export default EventAddToCalendar
--- app/src/Components/IconAndLabel.tsx
+++ app/src/Components/IconAndLabel.tsx
@@ -1,4 +1,6 @@
-// Displays a small icon (left) and a text label (to the right)
+/**
+ * @file Displays a small icon (left) and a text label (to the right).
+ */
 
 import React, { FC } from 'react'
 import Feather from 'react-native-vector-icons/Feather'
@@ -7,10 +9,11 @@ import ThemeVariables from '@/Theme/Variables'
 
 interface IconAndLabelProps {
   icon: string
+  onPress?: () => void
   text: string
 }
 
-const IconAndLabelView = styled.View`
+const IconAndLabelView = styled.TouchableOpacity`
   margin-right: 20px;
   margin-top: 9px;
   display: flex;
@@ -23,13 +26,20 @@ const IconAndLabelText = styled.Text`
   margin-left: 10px;
 `
 
-const IconAndLabel: FC<IconAndLabelProps> = ({ icon, text }) => {
-  return (
-    <IconAndLabelView>
-      <Feather name={icon} size={28} />
-      <IconAndLabelText>{text}</IconAndLabelText>
-    </IconAndLabelView>
-  )
-}
+/**
+ * Component to show an icon and a text label
+ *
+ * @param {IconAndLabelProps} props The component props
+ * @param {string} props.icon The name of the icon to use, from the React Native Vector Icons Feather library - see https://oblador.github.io/react-native-vector-icons/
+ * @param {function} [props.onPress] A function to run when the user taps on this component
+ * @param {string} props.text The text for the label
+ * @returns ReactElement Component
+ */
+const IconAndLabel: FC<IconAndLabelProps> = ({ icon, onPress, text }) => (
+  <IconAndLabelView onPress={onPress}>
+    <Feather name={icon} size={28} />
+    <IconAndLabelText>{text}</IconAndLabelText>
+  </IconAndLabelView>
+)
 
 export default IconAndLabel
