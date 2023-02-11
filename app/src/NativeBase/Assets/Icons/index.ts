49840b30859b0f1770516ccec1e39db1b1aefda1
PammyLo <pamela@tuminds.com>
2023-02-11 18:05:49 +0000

    fix ES Lint, mv component definition out of parent

--- app/src/NativeBase/Assets/Icons/index.ts
+++ app/src/NativeBase/Assets/Icons/index.ts
@@ -2,7 +2,7 @@
  * @file Exports icons for easy access elsewhere.
  */
 
-export { default as Home } from './Home.svg';
-export { default as Projects } from './Projects.svg';
-export { default as EventTicket } from './EventTicket.svg';
-export { default as User } from './User.svg';
+export { default as Home } from './Home.svg'
+export { default as Projects } from './Projects.svg'
+export { default as EventTicket } from './EventTicket.svg'
+export { default as User } from './User.svg'
--- app/src/Navigators/Main.tsx
+++ app/src/Navigators/Main.tsx
@@ -6,17 +6,82 @@ import React from 'react'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import { ListContainer } from '@/Containers'
 import { ListType } from '@/Containers/ListContainer'
-import { ProfileContainer, VerticalStackContainer} from '@/NativeBase/Containers'
+import {
+  ProfileContainer,
+  VerticalStackContainer
+} from '@/NativeBase/Containers'
 import { EventTicket, Home, Projects, User } from '@/NativeBase/Assets/Icons'
-import { View, useColorMode } from 'native-base'
+import { View, useColorMode, ColorMode } from 'native-base'
 import StaTheme from '@/NativeBase/Theme/StaTheme'
-
 
 const Tab = createBottomTabNavigator()
 
+const TabBarIconWrapper = ({ focused, colorMode }: { focused: boolean, colorMode: ColorMode }) => (
+  <View
+    borderTopColor={
+      focused
+        ? colorMode === 'light'
+          ? StaTheme.colors.primary['100']
+          : StaTheme.colors.primary['40']
+        : 'transparent'
+    }
+    borderTopWidth={2}
+    style={{ width: 97 }}
+  />
+)
+
+const iconColor = ({ focused, colorMode }: { focused: boolean, colorMode: ColorMode }) => {
+  return (
+  focused
+    ? colorMode === 'light'
+      ? StaTheme.colors.primary['100']
+      : StaTheme.colors.primary['40']
+    : colorMode === 'light'
+      ? StaTheme.colors.text['100']
+      : StaTheme.colors.textDarkMode['100']
+  )
+}
+
+const HomeTabBarIcon = ({ focused, colorMode }: { focused: boolean, colorMode: ColorMode }) => (
+  <>
+  <TabBarIconWrapper focused={ focused } colorMode={ colorMode } />
+  <View style={{ padding: 4 }} >
+      <Home
+        color={ iconColor({ focused, colorMode }) } />
+    </View>
+    </>
+)
+const ProjectsTabBarIcon = ({ focused, colorMode }: { focused: boolean, colorMode: ColorMode }) => (
+  <>
+  <TabBarIconWrapper focused={ focused } colorMode={ colorMode } />
+  <View style={{ padding: 9 }} >
+      <Projects
+        color={ iconColor({ focused, colorMode }) } />
+    </View>
+    </>
+)
+const EventsTabBarIcon = ({ focused, colorMode }: { focused: boolean, colorMode: ColorMode }) => (
+  <>
+  <TabBarIconWrapper focused={ focused } colorMode={ colorMode } />
+  <View style={{ padding: 7 }} >
+      <EventTicket
+        color={ iconColor({ focused, colorMode }) } />
+    </View>
+    </>
+)
+const ProfileTabBarIcon = ({ focused, colorMode }: { focused: boolean, colorMode: ColorMode }) => (
+  <>
+  <TabBarIconWrapper focused={ focused } colorMode={ colorMode } />
+  <View style={{ padding: 4 }} >
+      <User
+        color={ iconColor({ focused, colorMode }) } />
+    </View>
+    </>
+)
+  
 // @refresh reset
 const MainNavigator = () => {
-  const {colorMode} = useColorMode()
+  const { colorMode } = useColorMode()
 
   return (
     <Tab.Navigator>
@@ -26,31 +91,7 @@ const MainNavigator = () => {
         options=
         {{
           tabBarIcon: ({ focused }) => (
-            <>
-              <View
-                borderTopColor={focused
-                  ? (colorMode === 'light'
-                    ? StaTheme.colors.primary['100']
-                    : StaTheme.colors.primary['40'])
-                  : 'transparent'
-                }
-                borderTopWidth={2}
-                style={{ width: 97 }}
-              >
-              </View>
-              <View style={{ padding: 4 }}>
-                <Home
-                  color={focused
-                    ? (colorMode === 'light'
-                      ? StaTheme.colors.primary['100']
-                      : StaTheme.colors.primary['40'])
-                    : (colorMode === 'light'
-                      ? StaTheme.colors.text['100']
-                      : StaTheme.colors.textDarkMode['100'])
-                  }
-                />  
-              </View>
-            </>
+            <HomeTabBarIcon focused={focused} colorMode={colorMode} />
           )
         }}
       />
@@ -58,36 +99,11 @@ const MainNavigator = () => {
         name="Projects"
         component={ListContainer}
         initialParams={{ type: ListType.Projects }}
-        options={{
+        options=
+        {{
           headerShown: false,
           tabBarIcon: ({ focused }) => (
-            <>
-              <View
-                borderTopColor={focused
-                  ? (colorMode === 'light'
-                    ? StaTheme.colors.primary['100']
-                    : StaTheme.colors.primary['40'])
-                  : 'transparent'
-                }
-                borderTopWidth={2}
-                style={{ width: 97 }}
-              >
-              </View>
-              <View style={{
-                padding: 9
-              }} >
-                <Projects
-                  color={focused
-                    ? (colorMode === 'light'
-                      ? StaTheme.colors.primary['100']
-                      : StaTheme.colors.primary['40'])
-                    : (colorMode === 'light'
-                      ? StaTheme.colors.text['100']
-                      : StaTheme.colors.textDarkMode['100'])
-                  }
-                />
-              </View>
-            </>
+              <ProjectsTabBarIcon focused={focused} colorMode={colorMode} />
           )
         }}
       />
@@ -95,66 +111,18 @@ const MainNavigator = () => {
         name="Events"
         component={ListContainer}
         initialParams={{ type: ListType.Events }}
-        options={{
-          tabBarIcon: ({ focused }) => (
-            <>
-              <View
-                borderTopColor={focused
-                  ? (colorMode === 'light'
-                    ? StaTheme.colors.primary['100']
-                    : StaTheme.colors.primary['40'])
-                  : 'transparent'
-                }
-                borderTopWidth={2}
-                style={{ width: 97 }}
-              >
-              </View>
-              <View style={{ padding: 7 }}>
-                <EventTicket
-                  color={focused
-                    ? (colorMode === 'light'
-                      ? StaTheme.colors.primary['100']
-                      : StaTheme.colors.primary['40'])
-                    : (colorMode === 'light'
-                      ? StaTheme.colors.text['100']
-                      : StaTheme.colors.textDarkMode['100'])
-                  }
-                />
-              </View>
-            </>
+        options=
+        {{ tabBarIcon: ({ focused }) => (
+            <EventsTabBarIcon focused={focused} colorMode={colorMode} />
           )
         }}
       />
       <Tab.Screen
         name="Profile"
         component={ProfileContainer}
-        options={{
-          tabBarIcon: ({ focused }) => (
-            <>
-              <View
-                borderTopColor={ focused
-                  ? (colorMode === 'light'
-                    ? StaTheme.colors.primary['100']
-                    : StaTheme.colors.primary['40'])
-                  : 'transparent'
-                }
-                borderTopWidth={2}
-                style={{ width: 97 }}
-              >
-              </View>
-              <View style={{ padding: 4 }}>
-                <User
-                  color={focused
-                    ? (colorMode === 'light'
-                      ? StaTheme.colors.primary['100']
-                      : StaTheme.colors.primary['40'])
-                    : (colorMode === 'light'
-                      ? StaTheme.colors.text['100']
-                      : StaTheme.colors.textDarkMode['100'])
-                  }
-                />
-              </View>
-            </>
+        options=
+        {{ tabBarIcon: ({ focused }) => (
+            <ProfileTabBarIcon focused={focused} colorMode={colorMode} />
           )
         }}
           />
