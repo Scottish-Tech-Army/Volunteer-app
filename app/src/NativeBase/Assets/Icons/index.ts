7f1762d63a00bc199c7289125f0c1ccca682a801
PammyLo <pamela@tuminds.com>
2023-02-20 17:20:51 +0000

    fix icons and avoid defining component during render

--- app/src/NativeBase/Assets/Icons/index.ts
+++
@@ -1,8 +0,0 @@
-/**
- * @file Exports icons for easy access elsewhere.
- */
-
-export { default as Home } from './Home.svg'
-export { default as Projects } from './Projects.svg'
-export { default as EventTicket } from './EventTicket.svg'
-export { default as User } from './User.svg'
--- app/src/Navigators/Main.tsx
+++ app/src/Navigators/Main.tsx
@@ -2,114 +2,100 @@
  * @file Defines the main screens that have tabs at the bottom of the app, e.g. Projects, Events.
  */
 import React from 'react'
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import {
+  createBottomTabNavigator,
+  BottomTabNavigationOptions,
+} from '@react-navigation/bottom-tabs'
+import { useRoute } from '@react-navigation/native'
 import { ListContainer } from '@/Containers'
 import { ListType } from '@/Containers/ListContainer'
 import {
   ProfileContainer,
   VerticalStackContainer,
 } from '@/NativeBase/Containers'
-import { EventTicket, Home, Projects, User } from '@/NativeBase/Assets/Icons'
-import { View, useColorMode, ColorMode } from 'native-base'
-import StaTheme from '@/NativeBase/Theme/StaTheme'
 
-type navBarProps = {
-  focused: boolean
-  colorMode: ColorMode
-}
+import SelectionIcons from '@/NativeBase/Assets/Icons/SelectionIcons'
+import { Icon as IconifyIcon } from '@iconify/react'
+
+import { View, useColorMode } from 'native-base'
+import StaTheme from '@/NativeBase/Theme/StaTheme'
 
 const Tab = createBottomTabNavigator()
 
-const TabBarIconWrapper = ({ focused, colorMode }: navBarProps) => (
-  <View
-    borderTopColor={
-      focused
-        ? colorMode === 'light'
-          ? StaTheme.colors.primary['100']
-          : StaTheme.colors.primary['40']
-        : 'transparent'
-    }
-    borderTopWidth={2}
-    style={{
-      width: 97,
-    }}
-  />
-)
-
-const iconColor = ({ focused, colorMode }: navBarProps) => {
-  return focused
-    ? colorMode === 'light'
-      ? StaTheme.colors.primary['100']
-      : StaTheme.colors.primary['40']
-    : colorMode === 'light'
-    ? StaTheme.colors.text['100']
-    : StaTheme.colors.textDarkMode['100']
+interface CustomNavBarIconProps extends BottomTabNavigationOptions {
+  focused?: boolean
 }
 
-const HomeTabBarIcon = ({ focused, colorMode }: navBarProps) => (
-  <>
-    <TabBarIconWrapper focused={focused} colorMode={colorMode} />
+const NavBarIcon = ({ focused }: CustomNavBarIconProps) => {
+  const { colorMode } = useColorMode()
+  const route = useRoute()
+  let iconName = route.name
+
+  switch (route.name) {
+    case 'Home':
+      iconName = 'home'
+      break
+    case 'Projects':
+      iconName = 'six-dots'
+      break
+    case 'Events':
+      iconName = 'ticket'
+      break
+    case 'Profile':
+      iconName = 'user'
+      break
+  }
+
+  return (
     <View
+      borderTopColor={
+        focused
+          ? colorMode === 'light'
+            ? StaTheme.colors.primary['100']
+            : StaTheme.colors.primary['40']
+          : 'transparent'
+      }
+      borderTopWidth={2}
       style={{
-        padding: 4,
+        flexDirection: 'row-reverse',
+        width: 97,
+        height: 38,
+        alignItems: 'center',
+        justifyContent: 'center',
       }}
     >
-      <Home color={iconColor({ focused, colorMode })} />
+      <SelectionIcons
+        name={iconName}
+        color={
+          focused
+            ? colorMode === 'light'
+              ? StaTheme.colors.primary['100']
+              : StaTheme.colors.primary['40']
+            : colorMode === 'light'
+            ? StaTheme.colors.text['100']
+            : StaTheme.colors.textDarkMode['100']
+        }
+        size={18}
+      />
     </View>
-  </>
-)
-const ProjectsTabBarIcon = ({ focused, colorMode }: navBarProps) => (
-  <>
-    <TabBarIconWrapper focused={focused} colorMode={colorMode} />
-    <View
-      style={{
-        padding: 9,
-      }}
-    >
-      <Projects color={iconColor({ focused, colorMode })} />
-    </View>
-  </>
-)
-const EventsTabBarIcon = ({ focused, colorMode }: navBarProps) => (
-  <>
-    <TabBarIconWrapper focused={focused} colorMode={colorMode} />
-    <View
-      style={{
-        padding: 7,
-      }}
-    >
-      <EventTicket color={iconColor({ focused, colorMode })} />
-    </View>
-  </>
-)
-const ProfileTabBarIcon = ({ focused, colorMode }: navBarProps) => (
-  <>
-    <TabBarIconWrapper focused={focused} colorMode={colorMode} />
-    <View
-      style={{
-        padding: 4,
-      }}
-    >
-      <User color={iconColor({ focused, colorMode })} />
-    </View>
-  </>
-)
+  )
+}
 
 /**
  * @returns {import('@react-navigation/bottom-tabs').BottomTabNavigator} A bottom tab navigator component from the '@react-navigation/bottom-tabs' package
  */
 const MainNavigator = () => {
-  const { colorMode } = useColorMode()
+  const tabScreenOptions = {
+    tabBarIcon: props => NavBarIcon(props),
+  } as BottomTabNavigationOptions
 
   return (
     <Tab.Navigator>
       <Tab.Screen
-        name="Example Container"
+        name="Home"
         component={VerticalStackContainer}
         options={{
-          tabBarIcon: ({ focused }) => (
-            <HomeTabBarIcon focused={focused} colorMode={colorMode} />
-          ),
+          ...tabScreenOptions,
         }}
       />
       <Tab.Screen
@@ -118,9 +104,7 @@ const MainNavigator = () => {
         initialParams={{ type: ListType.Projects }}
         options={{
           headerShown: false,
-          tabBarIcon: ({ focused }) => (
-            <ProjectsTabBarIcon focused={focused} colorMode={colorMode} />
-          ),
+          ...tabScreenOptions,
         }}
       />
       <Tab.Screen
@@ -128,18 +112,14 @@ const MainNavigator = () => {
         component={ListContainer}
         initialParams={{ type: ListType.Events }}
         options={{
-          tabBarIcon: ({ focused }) => (
-            <EventsTabBarIcon focused={focused} colorMode={colorMode} />
-          ),
+          ...tabScreenOptions,
         }}
       />
       <Tab.Screen
         name="Profile"
         component={ProfileContainer}
         options={{
-          tabBarIcon: ({ focused }) => (
-            <ProfileTabBarIcon focused={focused} colorMode={colorMode} />
-          ),
+          ...tabScreenOptions,
         }}
       />
     </Tab.Navigator>
