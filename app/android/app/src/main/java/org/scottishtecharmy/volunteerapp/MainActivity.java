133697b5456d0d5815ccc8ba392893fe85bc811d
Dougie Richardson <dougie.richardson@outlook.com>
2023-03-05 18:53:38 +0000

    Component has to be registered as "main" for Expo

--- app/android/app/src/main/java/org/scottishtecharmy/volunteerapp/MainActivity.java
+++ app/android/app/src/main/java/org/scottishtecharmy/volunteerapp/MainActivity.java
@@ -15,7 +15,7 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "STA";
+    return "main";
   }
 
   // for react-native-screens
