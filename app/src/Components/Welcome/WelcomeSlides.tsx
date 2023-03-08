5fe5ce9207c8b0185c40a5a38cf34173612caf2f
BushraFatimaBF <vglampard@protonmail.com>
2023-03-08 14:31:03 +0000

    Changed lorem impsum text to STA website sentences

--- app/src/Components/Welcome/WelcomeSlides.tsx
+++ app/src/Components/Welcome/WelcomeSlides.tsx
@@ -1,35 +1,32 @@
 //Data to be rendered in the flatlist 'WelcomeSlider'
 
 import React from 'react'
-import {Image, View} from 'react-native'
+import { Image, View } from 'react-native'
 import Welcome from '@/Assets/Images/Welcome.png'
 import Volunteer from '@/Assets/Images/Volunteer.png'
 import MakeAnImpact from '@/Assets/Images/MakeAnImpact.png'
 import styled from 'styled-components/native'
-
 
 const WelcomeImage = styled.Image`
     height:55%;
 `
 
 export function WelcomeSlides() {
-    
-    return [
-        {
-            image:<WelcomeImage source={Welcome} resizeMode={'contain'}></WelcomeImage>,
-            title: "Welcome",
-            text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus eu nisl vitae nisl lobortis rutrum."
-        },
-        {
-            image:<WelcomeImage source={Volunteer} resizeMode={'contain'}></WelcomeImage>,
-            title: "Volunteer",
-            text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus eu nisl vitae nisl lobortis rutrum."
-        },
-        {
-            image:<WelcomeImage source={MakeAnImpact} resizeMode={'contain'}></WelcomeImage>,
-            title: "Make an Impact",
-            text: "In the first year of the STA our volunteers saved the third sector in Scotland over £1m"
-        }
-
-    ]
+  return [
+    {
+      image: <WelcomeImage source={Welcome} resizeMode={'contain'} />,
+      title: 'Welcome',
+      text: "Mobilising Scotland's tech talent for good...",
+    },
+    {
+      image: <WelcomeImage source={Volunteer} resizeMode={'contain'} />,
+      title: 'Volunteer',
+      text: 'We work in partnership with the technology ecosystem of Scotland to deliver scalable, impactful solutions.',
+    },
+    {
+      image: <WelcomeImage source={MakeAnImpact} resizeMode={'contain'} />,
+      title: 'Make an Impact',
+      text: 'In the first year of the STA our volunteers saved the third sector in Scotland over £1m',
+    },
+  ]
 }
