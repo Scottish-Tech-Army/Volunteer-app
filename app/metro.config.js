869804b24349a6fdd89016e4555cf40c1dde66ce
BushraFatimaBF <vglampard@protonmail.com>
2023-03-09 15:10:01 +0000

    removed , 'cjs', 'json' line from metro.config

--- app/metro.config.js
+++ app/metro.config.js
@@ -17,7 +17,7 @@ module.exports = (async () => {
     },
     resolver: {
       assetExts: assetExts.filter(ext => ext !== 'svg'),
-      sourceExts: [...sourceExts, 'svg', 'cjs', 'json'],
+      sourceExts: [...sourceExts, 'svg'],
     },
   }
 })()
