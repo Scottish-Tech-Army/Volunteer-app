c5b784ef490470041f1fb7ca20ceaca80706414e
Dougie Richardson <dougie.richardson@outlook.com>
2023-03-17 19:35:31 +0000

    Run cron jobs from server.

--- api/server.js
+++ api/server.js
@@ -1,5 +1,7 @@
 const app = require("./app");
+const cron = require('./cron-jobs/run-cron-jobs')
 
 app.listen(3000, () => {
+  cron
   console.log("Volunteer App API is listening on port 3000");
 });
