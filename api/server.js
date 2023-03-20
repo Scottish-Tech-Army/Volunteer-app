b676a783b8950f0d4da9ee0c7dfda0d9721a71c9
Dougie Richardson <me@dougiewougie.com>
2023-03-20 19:45:51 +0000

    Missing semicolon and updated README

--- api/server.js
+++ api/server.js
@@ -2,6 +2,6 @@ const app = require("./app");
 const cron = require('./cron-jobs/run-cron-jobs')
 
 app.listen(3000, () => {
-  cron
+  cron;
   console.log("Volunteer App API is listening on port 3000");
 });
