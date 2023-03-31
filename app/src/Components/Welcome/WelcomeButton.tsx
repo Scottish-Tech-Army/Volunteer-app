cdc16021edfcc789071151a6bffcd01334df490b
Kelsie Murphy <kelsie@kelsiesmurphy.com>
2023-03-31 14:22:18 +0100

    prettier-changes-for-eslint-checks

--- app/src/Components/Welcome/WelcomeButton.tsx
+++ app/src/Components/Welcome/WelcomeButton.tsx
@@ -1,7 +1,7 @@
 //'Get Started' button allows user to go to main part of app from any slide
 
 import React from 'react'
-import {TouchableOpacity, Text} from 'react-native'
+import { TouchableOpacity, Text } from 'react-native'
 import styled from 'styled-components/native'
 import { navigateAndSimpleReset } from '@/Navigators/utils'
 import { useTheme } from '@/Hooks'
@@ -9,13 +9,13 @@ import { useDispatch } from 'react-redux'
 import { changeWelcome, WelcomeState } from '@/Store/Welcome'
 
 const WelcomeButton = () => {
-    const { Colors, Fonts } = useTheme()
-    const dispatch = useDispatch()
-    const onChangeSplash = ({ welcome, show }: Partial<WelcomeState>) => {
-        dispatch(changeWelcome({ welcome, show }))
-    }
+  const { Colors, Fonts } = useTheme()
+  const dispatch = useDispatch()
+  const onChangeSplash = ({ welcome, show }: Partial<WelcomeState>) => {
+    dispatch(changeWelcome({ welcome, show }))
+  }
 
-    const WelcomeButton = styled.TouchableOpacity`
+  const WelcomeButton = styled.TouchableOpacity`
         width:244px;
         height:42px;
         borderRadius:8px;
@@ -26,23 +26,26 @@ const WelcomeButton = () => {
         marginTop:20px;
     `
 
-    const GetStartedText = styled.Text`
+  const GetStartedText = styled.Text`
         color:#ffffff;
         lineHeight:24px;
         fontSize:18px;
         fontWeight:400;
     `
 
-    const handlePress = () => {
-        onChangeSplash({ show: false })
-        navigateAndSimpleReset('Main')
-    }
+  const handlePress = () => {
+    onChangeSplash({ show: false })
+    navigateAndSimpleReset('Main')
+  }
 
-    return (
-        <WelcomeButton style={{backgroundColor:Colors.welcomeButton}} onPress={handlePress}>
-            <GetStartedText style={[Fonts.poppins]}>Get Started</GetStartedText>
-        </WelcomeButton>
-    )
+  return (
+    <WelcomeButton
+      style={{ backgroundColor: Colors.welcomeButton }}
+      onPress={handlePress}
+    >
+      <GetStartedText style={[Fonts.poppins]}>Get Started</GetStartedText>
+    </WelcomeButton>
+  )
 }
 
 export default WelcomeButton
