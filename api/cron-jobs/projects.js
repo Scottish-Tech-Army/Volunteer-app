232f749f8bfcbb251077df242cb9307cb6301aaa
Kelsie Murphy <kelsie@kelsiesmurphy.com>
2023-04-03 13:55:48 +0100

    Adding try-catch blocks to projects and removing axios call from events

--- api/cron-jobs/projects.js
+++ api/cron-jobs/projects.js
@@ -1,17 +1,17 @@
-require('dotenv').config();
-const airTable = require('../helpers/airTable');
-const arraysHelpers = require('../helpers/arrays');
-const axios = require('axios').default;
-const projectsHelpers = require('../helpers/projects');
-const videosService = require('../services/videos');
+require("dotenv").config();
+const airTable = require("../helpers/airTable");
+const arraysHelpers = require("../helpers/arrays");
+const axios = require("axios").default;
+const projectsHelpers = require("../helpers/projects");
+const videosService = require("../services/videos");
 const api_key = process.env.JIRA_API_KEY;
 const email = process.env.JIRA_EMAIL;
-const resourcingJiraBoardName = 'RES';
-const recruiterAssignedJiraColumnName = 'Recruiter Assigned';
-const projectJiraBoardName = 'IT';
-const volunteerSearch = 'Volunteer Search';
-const volunteerIntroduction = 'Volunteer Introduction';
-const activityUnderway = 'Activity Underway';
+const resourcingJiraBoardName = "RES";
+const recruiterAssignedJiraColumnName = "Recruiter Assigned";
+const projectJiraBoardName = "IT";
+const volunteerSearch = "Volunteer Search";
+const volunteerIntroduction = "Volunteer Introduction";
+const activityUnderway = "Activity Underway";
 
 async function addNewProjectsResources(projectsResources) {
   // AirTable accepts creating records in groups of 10 (faster than doing just one record at at time),
@@ -19,12 +19,15 @@ async function addNewProjectsResources(projectsResources) {
 
   const projectsResourcesChunked = arraysHelpers.chunk(projectsResources, 10);
 
-  console.log('🛈 Saving project resources');
+  console.log("🛈 Saving project resources");
   for (const projectsResourcesChunk of projectsResourcesChunked) {
-    await module.exports.addNewRecords(airTable.projectsResourcesCacheTable(), projectsResourcesChunk);
+    await module.exports.addNewRecords(
+      airTable.projectsResourcesCacheTable(),
+      projectsResourcesChunk
+    );
   }
 
-  console.log('🛈 Finished saving new cache data');
+  console.log("🛈 Finished saving new cache data");
 }
 
 async function addNewRecords(tableName, recordsChunk) {
@@ -32,24 +35,36 @@ async function addNewRecords(tableName, recordsChunk) {
     fields: record,
   }));
 
-  await airTable.client().table(tableName).create(recordsChunkFormattedForAirTable);
+  await airTable
+    .client()
+    .table(tableName)
+    .create(recordsChunkFormattedForAirTable);
 }
 
 async function cacheProjectsAndResources(projects, resources) {
   if (!projects.length || !resources.length) {
-    console.error('❌ No projects/resources returned from Jira API, so aborted updating cache');
+    console.error(
+      "❌ No projects/resources returned from Jira API, so aborted updating cache"
+    );
 
     return;
   }
 
-  console.log('🛈 Attempting to cache new data');
+  console.log("🛈 Attempting to cache new data");
 
-  const projectsResources = projectsHelpers.combineProjectsAndResources(projects, resources);
+  const projectsResources = projectsHelpers.combineProjectsAndResources(
+    projects,
+    resources
+  );
 
   try {
-    await module.exports.deleteAllRecords(airTable.projectsResourcesCacheTable());
+    await module.exports.deleteAllRecords(
+      airTable.projectsResourcesCacheTable()
+    );
   } catch (error) {
-    console.error('❌ Could not delete existing projects/resources records in cache, so aborted updating cache');
+    console.error(
+      "❌ Could not delete existing projects/resources records in cache, so aborted updating cache"
+    );
 
     return;
   }
@@ -57,19 +72,23 @@ async function cacheProjectsAndResources(projects, resources) {
   try {
     await module.exports.addNewProjectsResources(projectsResources);
   } catch (error) {
-    console.error('❌ Could not save new projects/resources records in cache');
+    console.error("❌ Could not save new projects/resources records in cache");
 
     return;
   }
 
-  console.log('🏁 Complete!');
+  console.log("🏁 Complete!");
 }
 
 async function deleteAllRecords(tableName) {
   console.log(`🛈 Deleting old records from ${tableName}`);
 
   return new Promise(async (resolve, reject) => {
-    const allRecordsRaw = await airTable.client().table(tableName).select().all();
+    const allRecordsRaw = await airTable
+      .client()
+      .table(tableName)
+      .select()
+      .all();
 
     // AirTable accepts creating records in groups of 10 (faster than doing just one record at at time),
     // so we chunk our data into an array of arrays, where each top-level array item is an array of up to 10 records
@@ -98,28 +117,34 @@ async function deleteRecords(tableName, recordIds) {
       .table(tableName)
       .destroy(recordIds, (error) => {
         if (error) {
-          console.error('❌ AirTable delete error', error);
-
+          console.error("❌ AirTable delete error", error);
           reject();
         }
-
         resolve();
       });
   });
 }
 
 function filterProjectsConnectedWithResources(itArray, resArray) {
-  return itArray.filter((project) => resArray.some((resource) => resource.it_key === project.it_key));
+  return itArray.filter((project) =>
+    resArray.some((resource) => resource.it_key === project.it_key)
+  );
 }
 
 function filterResourcesConnectedWithProjects(itArray, resArray) {
-  return resArray.filter((resource) => itArray.some((project) => project.it_key === resource.it_key));
+  return resArray.filter((resource) =>
+    itArray.some((project) => project.it_key === resource.it_key)
+  );
 }
 
 function formatProjects(projects, resources) {
   return projects.map((project) => {
-    const resourcesConnectedToProject = resources.filter((resource) => resource.it_key === project.it_key);
-    const projectType = resourcesConnectedToProject.length ? resourcesConnectedToProject[0].type : '';
+    const resourcesConnectedToProject = resources.filter(
+      (resource) => resource.it_key === project.it_key
+    );
+    const projectType = resourcesConnectedToProject.length
+      ? resourcesConnectedToProject[0].type
+      : "";
 
     return {
       ...project,
@@ -129,22 +154,33 @@ function formatProjects(projects, resources) {
 }
 
 async function getAllProjectsAndResourcesFromJira() {
-  console.log('🛈 Getting data from Jira and Vimeo APIs - this can take 5 seconds or more');
+  console.log(
+    "🛈 Getting data from Jira and Vimeo APIs - this can take 5 seconds or more"
+  );
 
   return new Promise((resolve) => {
-    const callAllItData = Promise.resolve(module.exports.getInitialTriageProjectsFromJira(0, []));
-    const callAllResData = Promise.resolve(module.exports.getResourcesFromJira(0, []));
+    const callAllItData = Promise.resolve(
+      module.exports.getInitialTriageProjectsFromJira(0, [])
+    );
+    const callAllResData = Promise.resolve(
+      module.exports.getResourcesFromJira(0, [])
+    );
 
     Promise.all([callAllItData, callAllResData]).then((data) => {
       const itArray = data[0];
       const resArray = data[1];
 
-      const projectsFiltered = module.exports.filterProjectsConnectedWithResources(itArray, resArray);
-      const resourcesFiltered = module.exports.filterResourcesConnectedWithProjects(itArray, resArray);
-      const projectsFilteredAndFormatted = module.exports.formatProjects(projectsFiltered, resourcesFiltered);
+      const projectsFiltered =
+        module.exports.filterProjectsConnectedWithResources(itArray, resArray);
+      const resourcesFiltered =
+        module.exports.filterResourcesConnectedWithProjects(itArray, resArray);
+      const projectsFilteredAndFormatted = module.exports.formatProjects(
+        projectsFiltered,
+        resourcesFiltered
+      );
 
       console.log(
-        `🛈 Found ${projectsFilteredAndFormatted.length} projects matching resources, ${resourcesFiltered.length} resources matching projects`,
+        `🛈 Found ${projectsFilteredAndFormatted.length} projects matching resources, ${resourcesFiltered.length} resources matching projects`
       );
 
       resolve({
@@ -157,80 +193,104 @@ async function getAllProjectsAndResourcesFromJira() {
 
 async function getInitialTriageProjectsFromJira(startAt, itArray) {
   const itJqlQuery = encodeURIComponent(
-    `project=${projectJiraBoardName} AND status="${volunteerSearch}" OR status="${volunteerIntroduction}" OR status="${activityUnderway}"`,
+    `project=${projectJiraBoardName} AND status="${volunteerSearch}" OR status="${volunteerIntroduction}" OR status="${activityUnderway}"`
   );
-  const jiraIt = await axios.get(
-    `https://sta2020.atlassian.net/rest/api/2/search?jql=${itJqlQuery}&startAt=${startAt}&maxResults=1000`,
-    {
-      headers: {
-        Authorization: `Basic ${Buffer.from(
-          // below use email address you used for jira and generate token from jira
-          `${email}:${api_key}`,
-        ).toString('base64')}`,
-        Accept: 'application/json',
-      },
-    },
-  );
+
+  let jiraIt;
+  try {
+    jiraIt = await axios.get(
+      `https://sta2020.atlassian.net/rest/api/2/search?jql=${itJqlQuery}&startAt=${startAt}&maxResults=1000`,
+      {
+        headers: {
+          Authorization: `Basic ${Buffer.from(
+            // below use email address you used for jira and generate token from jira
+            `${email}:${api_key}`
+          ).toString("base64")}`,
+          Accept: "application/json",
+        },
+      }
+    );
+  } catch (error) {
+    console.error(error);
+    return {
+      error: error.message,
+    };
+  }
 
   const itTotalData = parseInt(jiraIt.data.total);
 
+  // Does this need an if statement or similar to check if jiraIt is undefined?
   await Promise.all(
     jiraIt.data.issues.map(async (x) => {
       const project = {
-        it_key: x['key'],
-        name: x['fields'].summary,
-        description: x['fields'].description,
-        client: x['fields'].customfield_10027,
-        video_webpage: x['fields'].customfield_10159 ?? '',
-        scope: x['fields'].customfield_10090,
-        sector: x['fields'].customfield_10148?.value ?? '',
+        it_key: x["key"],
+        name: x["fields"].summary,
+        description: x["fields"].description,
+        client: x["fields"].customfield_10027,
+        video_webpage: x["fields"].customfield_10159 ?? "",
+        scope: x["fields"].customfield_10090,
+        sector: x["fields"].customfield_10148?.value ?? "",
       };
-
-      project.video_webpage_player_only = await videosService.getVideoWebpagePlayerOnly(project.video_webpage);
+      project.video_webpage_player_only =
+        await videosService.getVideoWebpagePlayerOnly(project.video_webpage);
 
       itArray.push(project);
-    }),
+    })
   );
 
   if (itArray.length < itTotalData) {
     const itStartResultSearch = itArray.length;
-
-    return module.exports.getInitialTriageProjectsFromJira(itStartResultSearch, itArray);
+    return module.exports.getInitialTriageProjectsFromJira(
+      itStartResultSearch,
+      itArray
+    );
   }
-
   return itArray;
 }
 
 async function getResourcesFromJira(startAt, resArray) {
   const resJqlQuery = encodeURIComponent(
-    `project=${resourcingJiraBoardName} AND status="${recruiterAssignedJiraColumnName}"`,
+    `project=${resourcingJiraBoardName} AND status="${recruiterAssignedJiraColumnName}"`
   );
-  const jiraRes = await axios.get(
-    `https://sta2020.atlassian.net/rest/api/2/search?jql=${resJqlQuery}&startAt=${startAt}&maxResults=1000`,
-    {
-      headers: {
-        Authorization: `Basic ${Buffer.from(
-          // below use email address you used for jira and generate token from jira
-          `${email}:${api_key}`,
-        ).toString('base64')}`,
-        Accept: 'application/json',
-      },
-    },
-  );
+  let jiraRes;
+  try {
+    jiraRes = await axios.get(
+      `https://sta2020.atlassian.net/rest/api/2/search?jql=${resJqlQuery}&startAt=${startAt}&maxResults=1000`,
+      {
+        headers: {
+          Authorization: `Basic ${Buffer.from(
+            // below use email address you used for jira and generate token from jira
+            `${email}:${api_key}`
+          ).toString("base64")}`,
+          Accept: "application/json",
+        },
+      }
+    );
+  } catch (error) {
+    console.error(error);
+    return {
+      error: error.message,
+    };
+  }
 
   const resTotalData = parseInt(jiraRes.data.total);
 
   jiraRes.data.issues.map((x) =>
     resArray.push({
-      res_id: x['id'],
-      it_key: x['fields'].customfield_10109,
-      type: x['fields'].customfield_10112,
-      role: x['fields'].customfield_10113,
-      skills: x['fields'].customfield_10061 ?? '',
-      hours: x['fields'].customfield_10165?.value ?? x['fields'].customfield_10062 ?? '', // customfield_10165 is the newer field, customfield_10062 may not be needed in the future
+      res_id: x["id"],
+      it_key: x["fields"].customfield_10109,
+      type: x["fields"].customfield_10112,
+      role: x["fields"].customfield_10113,
+      skills: x["fields"].customfield_10061 ?? "",
+      hours:
+        x["fields"].customfield_10165?.value ??
+        x["fields"].customfield_10062 ??
+        "", // customfield_10165 is the newer field, customfield_10062 may not be needed in the future
       required: 1, // currently hardcoded as cannot see number of people coming back in Jira results
-      buddying: x['fields'].customfield_10108 ? x['fields'].customfield_10108.value.toLowerCase() === 'yes' : false,
-    }),
+      buddying: x["fields"].customfield_10108
+        ? x["fields"].customfield_10108.value.toLowerCase() === "yes"
+        : false,
+    })
   );
 
   if (resArray.length < resTotalData) {
@@ -245,8 +305,12 @@ async function getResourcesFromJira(startAt, resArray) {
 async function startCachingLatestFromJira() {
   console.log(`🚀 Started caching projects and resources at ${new Date()}`);
 
-  const allProjectsAndResources = await module.exports.getAllProjectsAndResourcesFromJira();
-  module.exports.cacheProjectsAndResources(allProjectsAndResources.projects, allProjectsAndResources.resources);
+  const allProjectsAndResources =
+    await module.exports.getAllProjectsAndResourcesFromJira();
+  module.exports.cacheProjectsAndResources(
+    allProjectsAndResources.projects,
+    allProjectsAndResources.resources
+  );
 }
 
 module.exports = {
