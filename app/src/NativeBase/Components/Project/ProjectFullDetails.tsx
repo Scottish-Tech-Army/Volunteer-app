98ebdd8dab928d6ee134efff5c3dc94c6a1e9040
Dougie Richardson <me@dougiewougie.com>
2023-04-23 15:55:59 +0100

    Moved Button outside ScrollView and changed margin

--- app/src/NativeBase/Components/Project/ProjectFullDetails.tsx
+++ app/src/NativeBase/Components/Project/ProjectFullDetails.tsx
@@ -35,60 +35,61 @@ const ProjectFullDetails = ({
   project,
 }: ProjectFullDetailsProps): JSX.Element => {
   return (
-    <ScrollView
-      _dark={{ backgroundColor: 'bgDarkMode.100' }}
-      _light={{ backgroundColor: 'bg.secondary' }}
-    >
-      <VStack space={4}>
-        <HStack
-          justifyContent="space-between"
-          alignItems="center"
-          paddingRight="4"
-        >
-          <Heading width="70%" fontSize="md">
-            {project.name}
-          </Heading>
-          <ShareIcon
-            size="lg"
-            color="accentGrey.100"
-            onPress={underDevelopmentAlert}
+    <>
+      <ScrollView
+        _dark={{ backgroundColor: 'bgDarkMode.100' }}
+        _light={{ backgroundColor: 'bg.secondary' }}
+      >
+        <VStack space={4}>
+          <HStack
+            justifyContent="space-between"
+            alignItems="center"
+            paddingRight="4"
+          >
+            <Heading width="70%" fontSize="md">
+              {project.name}
+            </Heading>
+            <ShareIcon
+              size="lg"
+              color="accentGrey.100"
+              onPress={underDevelopmentAlert}
+            />
+            <FavouriteIcon
+              size="lg"
+              color="primary.100"
+              onPress={underDevelopmentAlert}
+            />
+          </HStack>
+
+          <Video
+            videoWebpage={project.video_webpage}
+            videoWebpagePlayerOnly={project.video_webpage_player_only}
+            videoWebpageScreen="ProjectVideo"
           />
-          <FavouriteIcon
-            size="lg"
-            color="primary.100"
-            onPress={underDevelopmentAlert}
-          />
-        </HStack>
 
-        <Video
-          videoWebpage={project.video_webpage}
-          videoWebpagePlayerOnly={project.video_webpage_player_only}
-          videoWebpageScreen="ProjectVideo"
-        />
-
-        <Card>
-          <Text>{project.client}</Text>
-          <ColouredTag title={project.role} />
-          <Text fontSize="xs">{project.hours}</Text>
-          <Text fontSize="xs" fontWeight="300">
-            {project.description}
-          </Text>
-          {Boolean(project.buddying) && (
-            <Text fontSize="xs">Suitable for pairing</Text>
-          )}
-          <ProjectAttachments details={'Project Scope'} url={project.scope} />
-        </Card>
-
-        <Button
-          marginBottom="12"
-          onPress={() => {
-            navigate('ProjectRegisterInterest', { project })
-          }}
-        >
-          Register
-        </Button>
-      </VStack>
-    </ScrollView>
+          <Card>
+            <Text>{project.client}</Text>
+            <ColouredTag title={project.role} />
+            <Text fontSize="xs">{project.hours}</Text>
+            <Text fontSize="xs" fontWeight="300">
+              {project.description}
+            </Text>
+            {Boolean(project.buddying) && (
+              <Text fontSize="xs">Suitable for pairing</Text>
+            )}
+            <ProjectAttachments details={'Project Scope'} url={project.scope} />
+          </Card>
+        </VStack>
+      </ScrollView>
+      <Button
+        margin={5}
+        onPress={() => {
+          navigate('ProjectRegisterInterest', { project })
+        }}
+      >
+        Register
+      </Button>
+    </>
   )
 }
 
