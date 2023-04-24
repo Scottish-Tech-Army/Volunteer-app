a98620515f10d61a26f72bd5c1c4ea2d7b3f7fb4
Kelsie Murphy <kelsie@kelsiesmurphy.com>
2023-04-24 18:00:53 +0100

    Removing unecessary jsx fragment

--- app/src/NativeBase/Components/SkeletonLoading.tsx
+++ app/src/NativeBase/Components/SkeletonLoading.tsx
@@ -7,27 +7,25 @@ import React from 'react'
 
 const SkeletonLoading = () => {
   return (
-    <>
-      <VStack
-        w="100%"
-        borderWidth="1"
-        space={4}
-        overflow="hidden"
-        rounded="md"
-        py="4"
-        px="2"
-        _dark={{
-          borderColor: 'coolGray.600',
-        }}
-        _light={{
-          borderColor: 'coolGray.200',
-        }}
-      >
-        <Skeleton />
-        <Skeleton.Text w="75%" lines={2} />
-        <Skeleton.Text lines={4} />
-      </VStack>
-    </>
+    <VStack
+      w="100%"
+      borderWidth="1"
+      space={4}
+      overflow="hidden"
+      rounded="md"
+      py="4"
+      px="2"
+      _dark={{
+        borderColor: 'coolGray.600',
+      }}
+      _light={{
+        borderColor: 'coolGray.200',
+      }}
+    >
+      <Skeleton />
+      <Skeleton.Text w="75%" lines={2} />
+      <Skeleton.Text lines={4} />
+    </VStack>
   )
 }
 
