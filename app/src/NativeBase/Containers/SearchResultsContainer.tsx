6dbff85b473f340e7abbb83fe3c036ace693e49e
Joe Derry Hall <joe@joederryhall.com>
2023-04-25 19:56:41 +0100

    Tidying up

--- app/src/NativeBase/Containers/SearchResultsContainer.tsx
+++ app/src/NativeBase/Containers/SearchResultsContainer.tsx
@@ -73,69 +73,63 @@ const SearchResultsContainer = (props: {
   }
 
   return (
-    <>
-      <Box>
+    <Box>
+      <View padding="4">
+        <HStack
+          alignItems="center"
+          backgroundColor="secondaryGrey.100"
+          borderBottomColor="primary.80"
+          borderBottomWidth="1"
+          borderRadius="4"
+          justifyContent="space-between"
+          onLayout={onLayoutEvent => {
+            const { width } = onLayoutEvent.nativeEvent.layout
+            setBoxWidth(width)
+          }}
+          paddingRight="1"
+          width="100%"
+        >
+          <Pressable onPress={goBack}>
+            <HStack
+              alignItems="center"
+              maxWidth={
+                boxWidth === '100%'
+                  ? boxWidth
+                  : (boxWidth as number) - clearButtonWidth
+              } // prevent longer text from spilling over and overlapping the close button
+            >
+              <SearchIcon />
+
+              {Boolean(params?.search.description) && (
+                <Text color="text.100" fontSize="sm">
+                  Results for {params.search.description}
+                </Text>
+              )}
+            </HStack>
+          </Pressable>
+
+          <IconButton
+            icon={
+              <Icon as={MaterialIcons} color="accentPurple.100" name="close" />
+            }
+            onPress={clearSearch}
+            size="xs"
+          />
+        </HStack>
+      </View>
+
+      {Boolean(params.search?.results) && (
         <View padding="4">
-          <HStack
-            alignItems="center"
-            backgroundColor="secondaryGrey.100"
-            borderBottomColor="primary.100"
-            borderBottomWidth="1"
-            borderRadius="4"
-            justifyContent="space-between"
-            onLayout={onLayoutEvent => {
-              const { width } = onLayoutEvent.nativeEvent.layout
-              setBoxWidth(width)
-            }}
-            paddingRight="1"
-            width="100%"
-          >
-            <Pressable onPress={goBack}>
-              <HStack
-                alignItems="center"
-                maxWidth={
-                  boxWidth === '100%'
-                    ? boxWidth
-                    : (boxWidth as number) - clearButtonWidth
-                } // prevent longer text from spilling over and overlapping the close button
-              >
-                <SearchIcon />
-
-                {Boolean(params?.search.description) && (
-                  <Text color="text.100" fontSize="sm">
-                    Results for {params.search.description}
-                  </Text>
-                )}
-              </HStack>
-            </Pressable>
-
-            <IconButton
-              icon={
-                <Icon
-                  as={MaterialIcons}
-                  color="accentPurple.100"
-                  name="close"
-                />
-              }
-              onPress={clearSearch}
-              size="xs"
-            />
-          </HStack>
+          <List
+            data={params.search?.results as Events | Projects}
+            mode={ListDisplayMode.Search}
+            options={params?.options}
+            searchScreen={searchScreens[params.type]}
+            type={params.type}
+          />
         </View>
-
-        {Boolean(params.search?.results) && (
-          <View padding="4">
-            <List
-              data={params.search?.results as Events | Projects}
-              mode={ListDisplayMode.Search}
-              options={params?.options}
-              searchScreen={searchScreens[params.type]}
-              type={params.type}
-            />
-          </View>
-        )}
-      </Box>
-    </>
+      )}
+    </Box>
   )
 }
 
