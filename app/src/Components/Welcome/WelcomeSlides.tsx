e935dfbb94067b94a45e9494ec090a78f58f1ea0
Victoria Lampard <vglampard@protonmail.com>
2023-05-03 19:57:50 +0100

    changed lorem ipsum text

--- app/src/Components/Welcome/WelcomeSlides.tsx
+++ app/src/Components/Welcome/WelcomeSlides.tsx
@@ -25,18 +25,18 @@ export function WelcomeSlides() {
   return [
     {
       image: <WelcomeImage source={Welcome} resizeMode={'contain'} />,
-      title: 'WELCOME',
-      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus eu nisl vitae nisl lobortis rutrum.',
+      title: 'Welcome',
+      text: "Mobilising the UK's tech talent for good...",
     },
     {
       image: <WelcomeImage source={Volunteer} resizeMode={'contain'} />,
-      title: 'VOLUNTEER',
-      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus eu nisl vitae nisl lobortis rutrum.',
+      title: 'Volunteer',
+      text: "We work in partnership with the UK's technology ecosystem to deliver scalable, impactful solutions.",
     },
     {
       image: <WelcomeImage source={MakeAnImpact} resizeMode={'contain'} />,
-      title: 'MAKE AN IMPACT',
-      text: 'In the first year of the STA our volunteers saved the third sector in Scotland over £1m',
+      title: 'Make an Impact',
+      text: 'In the first year of the STA our volunteers saved the third sector in the UK over £1m.',
     },
   ] as WelcomeSlide[]
 }
