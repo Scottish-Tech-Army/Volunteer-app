1a2e80c763064807805c6a3796e31900d0449564
Victoria Lampard <vglampard@protonmail.com>
2023-05-03 20:17:57 +0100

    changed the titles back to upper case

--- app/src/Components/Welcome/WelcomeSlides.tsx
+++ app/src/Components/Welcome/WelcomeSlides.tsx
@@ -25,18 +25,18 @@ export function WelcomeSlides() {
   return [
     {
       image: <WelcomeImage source={Welcome} resizeMode={'contain'} />,
-      title: 'Welcome',
+      title: 'WELCOME',
       text: "Mobilising the UK's tech talent for good...",
     },
     {
       image: <WelcomeImage source={Volunteer} resizeMode={'contain'} />,
-      title: 'Volunteer',
-      text: "We work in partnership with the UK's technology ecosystem to deliver scalable, impactful solutions.",
+      title: 'VOLUNTEER',
+      text: "We work in partnership with the UK's technology ecosystem to deliver scalable, impactful solutions",
     },
     {
       image: <WelcomeImage source={MakeAnImpact} resizeMode={'contain'} />,
-      title: 'Make an Impact',
-      text: 'In the first year of the STA our volunteers saved the third sector in the UK over £1m.',
+      title: 'MAKE AN IMPACT',
+      text: 'In the first year of the STA our volunteers saved the third sector in Scotland over £1m',
     },
   ] as WelcomeSlide[]
 }
