ae391d6192ccdf63fe8b8978d61bd79794df8607
Kelsie Murphy <kelsie@kelsiesmurphy.com>
2023-05-12 11:12:11 +0100

    Reverting manual changes created by accident

--- app/android/app/src/main/java/org/scottishtecharmy/volunteerapp/MainApplication.java
+++ app/android/app/src/main/java/org/scottishtecharmy/volunteerapp/MainApplication.java
@@ -11,8 +11,6 @@ import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
-import cl.json.RNSharePackage;
-import cl.json.ShareApplication;
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -29,7 +27,6 @@ public class MainApplication extends Application implements ReactApplication {
       // Packages that cannot be autolinked yet can be added manually here, for
       // example:
       // packages.add(new MyReactNativePackage());
-      packages.add(new RNSharePackage());
       return packages;
 
     }
