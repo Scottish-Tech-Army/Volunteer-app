ca6f57f41058402dd1b5f7381b846cdcabb1e41f
Joe Derry Hall <joe@joederryhall.com>
2023-05-31 14:48:57 +0100

    Fix Jest test

--- app/__tests__/App-test.js
+++ app/__tests__/App-test.js
@@ -1,9 +1,10 @@
 /**
+ * @file Jest test for App.tsx
  * @format
  */
 import 'react-native'
 import React from 'react'
-import App from '../src/App'
+import App from '../App'
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer'
