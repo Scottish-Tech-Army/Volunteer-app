ae017a2bd35dec5eb1b43fbd1f789cbec4c54047
Joe Derry Hall <joe@joederryhall.com>
2023-05-31 11:16:14 +0100

    Fix bug connecting to API from app in Expo Go

--- app/src/Services/api.ts
+++ app/src/Services/api.ts
@@ -23,8 +23,7 @@ const baseQueryWithInterceptor: ApiBaseQueryFunctionType = async (
   extraOptions,
 ) => {
   const result = await baseQuery(args, api, extraOptions)
-  if (result.error && result.error.status === 401) {
-  }
+
   return result
 }
 
