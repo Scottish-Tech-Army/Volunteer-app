3520598460f1e5daf29014d5ebea345aaff5d6ce
PammyLo <pamela@tuminds.com>
2023-06-14 19:51:13 +0100

    update regex

--- api/helpers/urls.js
+++ api/helpers/urls.js
@@ -1,8 +1,13 @@
+/**
+ * This function finds urls or emails that are wrapped in square brackets, i.e.
+ * [https://www.example.com|unwanted text] and [example@example|unwanted text/characters]
+ * It captures the url removing unwanted characters, and removes the emails completely
+ * the first part of the regex matches the url, the second part matches the email
+ */
+
 function cleanUrlsAndEmails(string) {
-  // this regex finds urls or emails in square brackets, and captures the url
-  // [https://www.example.com|unwanted text] and [example@example|unwanted text/characters]
   const urlAndEmailRegExPattern =
-    /\[(https?\S+?)\|.*?\]|\[[^\s@]+@[^\s@]+\.[^\s@]+\|.*?\]/g;
+    /\[(https?\S+?)\s*\|.*?\]|\[[^\s@]+@[^\s@]+\.[^\s@]+\|.*?\]/g;
 
   return string.replace(urlAndEmailRegExPattern, (match, url) =>
     url ? url : ""
