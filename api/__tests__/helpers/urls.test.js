6d21d72b5216b298eac24334f5bcb2d4b6d2780b
PammyLo <pamela@tuminds.com>
2023-06-14 20:37:07 +0100

    add test

--- api/__tests__/helpers/urls.test.js
+++ api/__tests__/helpers/urls.test.js
@@ -1,24 +1,33 @@
-const cleanUrlsAndEmails = require('../../helpers/urls').cleanUrlsAndEmails;
+const cleanUrlsAndEmails = require("../../helpers/urls").cleanUrlsAndEmails;
 
-describe('Test the clean-up urls and emails helper', () => {
-  it('should remove unwanted characters from a URL', () => {
-    const exampleUrl = '[https://example.com/|unwanted text]';
-    const expectedOutput = 'https://example.com/';
+describe("Test the clean-up urls and emails helper", () => {
+  it("should remove unwanted characters from a URL", () => {
+    const exampleUrl = "[https://example.com/|unwanted text]";
+    const expectedOutput = "https://example.com/";
     const formattedUrl = cleanUrlsAndEmails(exampleUrl);
     expect(formattedUrl).toEqual(expectedOutput);
   });
 
-  it('should detect urls and emails in text and clean up the url and remove the email', () => {
-    const text =
-      'This is some text with a url [https://example.com/|unwanted text] in it, and an email [test@example.com|mailto:test@example.com|unwanted text]';
+  it("should remove unwanted characters from a URL including an empty space", () => {
+    const exampleUrl =
+      "[https://trello.com/invite/b/dFhDtziM/ATTIce48aa21761d1237c7192b133fd4f0ba920D27E5/mentoring-app |https://trello.com/invite/b/dFhDtziM/ATTIce48aa21761d1237c7192b133fd4f0ba920D27E5/mentoring-app]";
     const expectedOutput =
-      'This is some text with a url https://example.com/ in it, and an email ';
+      "https://trello.com/invite/b/dFhDtziM/ATTIce48aa21761d1237c7192b133fd4f0ba920D27E5/mentoring-app";
+    const formattedUrl = cleanUrlsAndEmails(exampleUrl);
+    expect(formattedUrl).toEqual(expectedOutput);
+  });
+
+  it("should detect urls and emails in text and clean up the url and remove the email", () => {
+    const text =
+      "This is some text with a url [https://example.com/|unwanted text] in it, and an email [test@example.com|mailto:test@example.com|unwanted text]";
+    const expectedOutput =
+      "This is some text with a url https://example.com/ in it, and an email ";
     const formattedText = cleanUrlsAndEmails(text);
     expect(formattedText).toEqual(expectedOutput);
   });
 
-  it('should not remove anything if there are no URLs or emails in square brackets', () => {
-    const text = 'This is text with no URLs or emails.';
+  it("should not remove anything if there are no URLs or emails in square brackets", () => {
+    const text = "This is text with no URLs or emails.";
     const cleanedText = cleanUrlsAndEmails(text);
     expect(cleanedText).toStrictEqual(text);
   });
