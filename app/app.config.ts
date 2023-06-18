36052171452958f0b86a4db009d0ab5a36bdcca3
Joe Derry Hall <joe@joederryhall.com>
2023-06-18 16:11:16 +0100

    Update version numbers for deployment

--- app/app.config.ts
+++ app/app.config.ts
@@ -25,7 +25,7 @@ module.exports = {
         backgroundColor: '#ffffff',
       },
       package: 'org.scottishtecharmy.volunteerapp',
-      versionCode: 14,
+      versionCode: 15,
     },
     ios: {
       bundleIdentifier: 'org.scottishtecharmy.volunteerapp',
@@ -34,7 +34,7 @@ module.exports = {
         NSCalendarsUsageDescription:
           "The app will show tech/volunteering-related events that the user might be interested in. The user can choose to attend an event - if so, the app needs access to the user's calendar in order to add an event.",
       },
-      buildNumber: '16',
+      buildNumber: '17',
       supportsTablet: true,
     },
     web: {
