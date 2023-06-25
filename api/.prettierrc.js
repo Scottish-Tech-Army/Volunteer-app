c9386a71b3993a4c3691d76a53ee7398d805c8a2
Marcin-at-918147473843 <borowski1976@yahoo.com>
2023-06-25 22:28:38 +0100

    .prettier.js added also type module in package.json

---
+++ api/.prettierrc.js
@@ -0,0 +1,6 @@
+export const bracketSameLine = false;
+export const singleQuote = true;
+export const trailingComma = 'all';
+export const semi = false;
+export const arrowParens = 'avoid';
+export const embeddedLanguageFormatting = 'off';
