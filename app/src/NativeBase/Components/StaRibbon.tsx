fe54500111d243a670cbe3c187c6e078246e10f8
DJMF2015 <djm.fulton@gmail.com>
2023-07-18 15:02:13 +0100

    removed curly braces an not required

--- app/src/NativeBase/Components/StaRibbon.tsx
+++ app/src/NativeBase/Components/StaRibbon.tsx
@@ -4,17 +4,11 @@
 import { HStack, Box } from 'native-base'
 import React from 'react'
 
-const StaRibbon = () => {
-  return (
-    <HStack space="2">
-      <Box bgColor="primary.100" height="15" width="70%" />
-      <Box
-        bgColor="primary.100"
-        height="15"
-        width="5%"
-        borderRightRadius={10}
-      />
-    </HStack>
-  )
-}
+const StaRibbon = () => (
+  <HStack space="2">
+    <Box bgColor="primary.100" height="15" width="70%" />
+    <Box bgColor="primary.100" height="15" width="5%" borderRightRadius={10} />
+  </HStack>
+)
+
 export default StaRibbon
