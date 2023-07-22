78acfa17244b2e31dcedd47285a35d20953fd6da
DJMF2015 <djm.fulton@gmail.com>
2023-07-22 14:47:36 +0100

    modified component to take in bg color props to be used in other components

--- app/src/NativeBase/Components/ProgressBar.tsx
+++ app/src/NativeBase/Components/ProgressBar.tsx
@@ -1,16 +1,24 @@
 /**
  * @file for Sta Progress Bar component
  */
-import { HStack, Box } from 'native-base'
+import { Box, HStack } from 'native-base'
 import React from 'react'
 
-const ProgressBar = () => (
-  <HStack marginX={5} space="3">
-    <Box bgColor="purple.20" height="15" width="22%" />
-    <Box bgColor="secondaryGrey.80" height="15" width="22%" />
-    <Box bgColor="secondaryGrey.80" height="15" width="22%" />
-    <Box bgColor="secondaryGrey.80" height="15" width="22%" />
-  </HStack>
-)
+type ProgressBarProps = {
+  bgColor: string
+}
+
+const ProgressBar: React.FC<ProgressBarProps> = ({ bgColor }) => {
+  return (
+    <>
+      <HStack marginX={5} space="3">
+        <Box bgColor={bgColor} height="15" width="22%" />
+        <Box bgColor={'secondaryGrey.80'} height="15" width="22%" />
+        <Box bgColor="secondaryGrey.80" height="15" width="22%" />
+        <Box bgColor="secondaryGrey.80" height="15" width="22%" />
+      </HStack>
+    </>
+  )
+}
 
 export default ProgressBar
