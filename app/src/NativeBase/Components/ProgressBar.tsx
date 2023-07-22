276180cd006b0360a19d0e86225b53df66dd0995
DJMF2015 <djm.fulton@gmail.com>
2023-07-22 17:20:23 +0100

    added interface for box number and dynamic updating of box colour through props

--- app/src/NativeBase/Components/ProgressBar.tsx
+++ app/src/NativeBase/Components/ProgressBar.tsx
@@ -4,18 +4,23 @@
 import { Box, HStack } from 'native-base'
 import React from 'react'
 
-type ProgressBarProps = {
-  bgColor: string
+interface ProgressBarColors {
+  box1: string
+  box2: string
+  box3: string
+  box4: string
 }
 
-const ProgressBar: React.FC<ProgressBarProps> = ({ bgColor }) => {
+const ProgressBar: React.FC<{ progressBarColors: ProgressBarColors }> = ({
+  progressBarColors,
+}) => {
   return (
     <>
       <HStack marginX={5} space="3">
-        <Box bgColor={bgColor} height="15" width="22%" />
-        <Box bgColor={'secondaryGrey.80'} height="15" width="22%" />
-        <Box bgColor="secondaryGrey.80" height="15" width="22%" />
-        <Box bgColor="secondaryGrey.80" height="15" width="22%" />
+        <Box bgColor={progressBarColors.box1} height="15" width="22%" />
+        <Box bgColor={progressBarColors.box2} height="15" width="22%" />
+        <Box bgColor={progressBarColors.box3} height="15" width="22%" />
+        <Box bgColor={progressBarColors.box4} height="15" width="22%" />
       </HStack>
     </>
   )
