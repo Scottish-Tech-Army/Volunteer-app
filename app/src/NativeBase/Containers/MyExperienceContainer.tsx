9dfc60b50996fb0378081ceb6f7169cccf4a76bf
DJMF2015 <djm.fulton@gmail.com>
2023-07-24 10:07:25 +0100

    remove keyboard component

--- app/src/NativeBase/Containers/MyExperienceContainer.tsx
+++ app/src/NativeBase/Containers/MyExperienceContainer.tsx
@@ -17,7 +17,6 @@ import FreeSearchBar from '../Components/FreeSearchBar'
 import React from 'react'
 import Checkbox from '../Components/Checkbox'
 import { RoleGroup, roleGroups } from '@/Services/modules/projects/roleGroups'
-import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view'
 import {
   progressBarColors,
   changeScreen,
@@ -62,82 +61,81 @@ const MyExperienceContainer = () => {
           handleChangeText={(text: string) => setSearchTxt(text)}
         />
       </View>
-      <KeyboardAwareScrollView>
-        <ScrollView>
-          <VStack mt={-2} margin="5">
-            {filteredSkills.map((roleGroup: RoleGroup, index: number) => (
-              <View
-                key={`${roleGroup.groupName} - ${index}`}
-                flexDirection={'row'}
-                alignItems={'center'}
+
+      <ScrollView>
+        <VStack mt={-2} margin="5">
+          {filteredSkills.map((roleGroup: RoleGroup, index: number) => (
+            <View
+              key={`${roleGroup.groupName} - ${index}`}
+              flexDirection={'row'}
+              alignItems={'center'}
+            >
+              <Checkbox
+                onChange={() => {
+                  if (skillsValue.includes(roleGroup.groupName)) {
+                    setSkillsValue(
+                      skillsValue.filter(
+                        skill => skill !== roleGroup.groupName, // remove the skill from the array
+                      ),
+                    )
+                  } else {
+                    setSkillsValue([...skillsValue, roleGroup.groupName]) // add the skill to the array
+                  }
+                }}
+                checked={skillsValue.includes(roleGroup.groupName)}
+              />
+              <Text
+                ml="4"
+                marginY="0.5"
+                onPress={() => {
+                  if (skillsValue.includes(roleGroup.groupName)) {
+                    setSkillsValue(
+                      skillsValue.filter(
+                        skill => skill !== roleGroup.groupName,
+                      ),
+                    )
+                  } else {
+                    setSkillsValue([...skillsValue, roleGroup.groupName])
+                  }
+                }}
               >
-                <Checkbox
-                  onChange={() => {
-                    if (skillsValue.includes(roleGroup.groupName)) {
-                      setSkillsValue(
-                        skillsValue.filter(
-                          skill => skill !== roleGroup.groupName, // remove the skill from the array
-                        ),
-                      )
-                    } else {
-                      setSkillsValue([...skillsValue, roleGroup.groupName]) // add the skill to the array
-                    }
-                  }}
-                  checked={skillsValue.includes(roleGroup.groupName)}
-                />
-                <Text
-                  ml="4"
-                  marginY="0.5"
-                  onPress={() => {
-                    if (skillsValue.includes(roleGroup.groupName)) {
-                      setSkillsValue(
-                        skillsValue.filter(
-                          skill => skill !== roleGroup.groupName,
-                        ),
-                      )
-                    } else {
-                      setSkillsValue([...skillsValue, roleGroup.groupName])
-                    }
-                  }}
-                >
-                  {roleGroup.groupName}
-                </Text>
-              </View>
-            ))}
-          </VStack>
-          <VStack marginX={6}>
-            <Button
-              borderWidth={2}
-              backgroundColor="bg.100"
-              borderColor="purple.100"
-              _text={{
-                color: 'purple.100',
-              }}
-              onPress={() => changeScreen(currentBox, setCurrentBox)}
-              disabled={currentBox === 4}
-            >
-              Next
-            </Button>
-            <Button
-              borderWidth={2}
-              borderColor={'bg.100'}
-              backgroundColor="bg.100"
-              _dark={{
-                backgroundColor: 'bgDarkMode.100',
-                _text: { color: 'white' },
-                borderColor: 'white',
-              }}
-              _text={{
-                color: 'black',
-              }}
-              disabled={currentBox === 1}
-              onPress={() => goBackScreen(currentBox, setCurrentBox)}
-            >
-              Skip
-            </Button>
-          </VStack>
-        </ScrollView>
-      </KeyboardAwareScrollView>
+                {roleGroup.groupName}
+              </Text>
+            </View>
+          ))}
+        </VStack>
+        <VStack marginX={6}>
+          <Button
+            borderWidth={2}
+            backgroundColor="bg.100"
+            borderColor="purple.100"
+            _text={{
+              color: 'purple.100',
+            }}
+            onPress={() => changeScreen(currentBox, setCurrentBox)}
+            disabled={currentBox === 4}
+          >
+            Next
+          </Button>
+          <Button
+            borderWidth={2}
+            borderColor={'bg.100'}
+            backgroundColor="bg.100"
+            _dark={{
+              backgroundColor: 'bgDarkMode.100',
+              _text: { color: 'white' },
+              borderColor: 'white',
+            }}
+            _text={{
+              color: 'black',
+            }}
+            disabled={currentBox === 1}
+            onPress={() => goBackScreen(currentBox, setCurrentBox)}
+          >
+            Skip
+          </Button>
+        </VStack>
+      </ScrollView>
     </>
   )
 }
