7c910e636515f333be9a89f480b26dc614d0e31e
Rory Fitzpatrick <badgrs@gmail.com>
2023-10-16 23:20:51 +0100

    First pass at auth api

--- api/routes/v1.1/index.js
+++ api/routes/v1.1/index.js
@@ -1,11 +1,13 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const { eventsApi } = require('../events');
-const { projectsApi } = require('../projects');
-const { apiKeyMiddleware } = require('../apiKeyMiddleware');
+const { eventsApi } = require("../events");
+const { projectsApi } = require("../projects");
+const { apiKeyMiddleware } = require("../apiKeyMiddleware");
+const { authApi, middlewares: authMiddlewares } = require("../auth");
 
-router.use(apiKeyMiddleware)
-router.use('/projects', projectsApi);
-router.use('/events', eventsApi);
+router.use(apiKeyMiddleware);
+router.use("/projects", projectsApi);
+router.use("/events", eventsApi);
+router.use("/auth", authMiddlewares, authApi);
 
 module.exports = router;
