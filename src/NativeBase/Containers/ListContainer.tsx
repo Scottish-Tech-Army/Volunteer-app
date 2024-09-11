de3f8f6881bbdca4619fd36ac5e1cc38c9934097
mirkiy <mirizachar@gmail.com>
2024-09-11 18:55:12 +0100

    new filter button and horizontal scrollview added

--- src/NativeBase/Containers/ListContainer.tsx
+++ src/NativeBase/Containers/ListContainer.tsx
@@ -17,6 +17,7 @@ import {
   VStack,
   Text,
   Pressable,
+  View,
 } from 'native-base'
 import React, { useEffect, useState } from 'react'
 import { Dimensions } from 'react-native'
@@ -303,107 +304,147 @@ const ListContainer = (props: {
         showSearchButton
         onSearchButtonPress={() => navigate(screens.search[params.type], '')}
       /> */}
-      <VStack mt="4" px="4" space="2">
-        <Heading size="xl">Roles</Heading>
-        {/* TODO: reinstate when functionality is ready */}
-        {/* <SegmentedPicker options={projectListOptions} /> */}
-
-        <FreeSearchBar handleSubmit={onSubmitEditing} marginBottom="2" />
-      </VStack>
       <VStack space={2} alignItems="center">
-        <HStack space={2} alignItems="center">
-          <Pressable
-            onPress={() => handleTagPress('Roles')}
-            borderRadius={40}
-            p={2}
-            display="flex"
-            flexDirection="row"
-            alignItems="center"
-            justifyContent="center"
-            backgroundColor="#F6E2EE"
-            borderColor="#D1338A"
-            borderWidth={1}
-          >
-            <Text color="#D1338A">Roles</Text>
-            <IconButton
-              icon={
-                <Icon
-                  as={MaterialIcons}
-                  name={
-                    iconState.Roles
-                      ? 'keyboard-arrow-up'
-                      : 'keyboard-arrow-down'
-                  }
-                  size={5}
-                  color="#D1338A"
-                />
-              }
-              variant="outline"
-              _icon={{ color: 'gray.500' }}
+        <VStack mt="4" px="4">
+          <Heading size="xl">Roles</Heading>
+          <HStack space="2" alignItems="center" width="100%">
+            {/* TODO: reinstate when functionality is ready */}
+            {/* <SegmentedPicker options={projectListOptions} /> */}
+            <View flex={1}>
+              <FreeSearchBar handleSubmit={onSubmitEditing} marginBottom="2" />
+            </View>
+            <View
+              marginBottom="2"
+              height="48px"
+              width="48px"
+              bg="#F0F0F0"
+              borderRadius="10px"
+              padding="2"
+              mx="10px"
+              justifyContent="center"
+              alignItems="center"
+            >
+              <IconButton
+                icon={
+                  <Icon
+                    as={MaterialIcons}
+                    name="tune"
+                    size="24px"
+                    color="gray.500"
+                  />
+                }
+              />
+            </View>
+          </HStack>
+        </VStack>
+
+        <ScrollView
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          paddingTop="0px"
+        >
+          <HStack space={2} alignItems="center">
+            <Pressable
               onPress={() => handleTagPress('Roles')}
-            />
-          </Pressable>
-          <Pressable
-            onPress={() => handleTagPress('Tech')}
-            borderRadius={40}
-            p={2}
-            display="flex"
-            flexDirection="row"
-            alignItems="center"
-            justifyContent="center"
-            backgroundColor="#F6E2EE"
-            borderColor="#D1338A"
-            borderWidth={1}
-          >
-            <Text color="#D1338A">Tech</Text>
-            <IconButton
-              icon={
-                <Icon
-                  as={MaterialIcons}
-                  name={
-                    iconState.Tech ? 'keyboard-arrow-up' : 'keyboard-arrow-down'
-                  }
-                  size={5}
-                  color="#D1338A"
-                />
-              }
-              variant="outline"
-              _icon={{ color: 'gray.500' }}
+              borderRadius={40}
+              pl={4}
+              pr={2}
+              py={2}
+              display="flex"
+              flexDirection="row"
+              alignItems="center"
+              justifyContent="center"
+              backgroundColor="#F6E2EE"
+              borderColor="#D1338A"
+              borderWidth={1}
+            >
+              <Text color="#D1338A">Roles</Text>
+              <IconButton
+                icon={
+                  <Icon
+                    as={MaterialIcons}
+                    name={
+                      iconState.Roles
+                        ? 'keyboard-arrow-up'
+                        : 'keyboard-arrow-down'
+                    }
+                    size={5}
+                    color="#D1338A"
+                  />
+                }
+                variant="outline"
+                _icon={{ color: 'gray.500' }}
+                onPress={() => handleTagPress('Roles')}
+              />
+            </Pressable>
+            <Pressable
               onPress={() => handleTagPress('Tech')}
-            />
-          </Pressable>
-          <Pressable
-            onPress={() => handleTagPress('Causes')}
-            borderRadius={40}
-            p={2}
-            display="flex"
-            flexDirection="row"
-            alignItems="center"
-            justifyContent="center"
-            backgroundColor="#F6E2EE"
-            borderColor="#D1338A"
-            borderWidth={1}
-          >
-            <Text color="#D1338A">Causes</Text>
-            <IconButton
-              icon={
-                <Icon
-                  as={MaterialIcons}
-                  name={
-                    iconState.Causes
-                      ? 'keyboard-arrow-up'
-                      : 'keyboard-arrow-down'
-                  }
-                  size={5}
-                  color="#D1338A"
-                />
-              }
-              variant="outline"
-              _icon={{ color: 'gray.500' }}
+              borderRadius={40}
+              pl={4}
+              pr={2}
+              py={2}
+              display="flex"
+              flexDirection="row"
+              alignItems="center"
+              justifyContent="center"
+              backgroundColor="#F6E2EE"
+              borderColor="#D1338A"
+              borderWidth={1}
+            >
+              <Text color="#D1338A">Tech</Text>
+              <IconButton
+                icon={
+                  <Icon
+                    as={MaterialIcons}
+                    name={
+                      iconState.Tech
+                        ? 'keyboard-arrow-up'
+                        : 'keyboard-arrow-down'
+                    }
+                    size={5}
+                    color="#D1338A"
+                  />
+                }
+                variant="outline"
+                _icon={{ color: 'gray.500' }}
+                onPress={() => handleTagPress('Tech')}
+              />
+            </Pressable>
+            <Pressable
               onPress={() => handleTagPress('Causes')}
-            />
-          </Pressable>
-        </HStack>
+              borderRadius={40}
+              pl={4}
+              pr={2}
+              py={2}
+              display="flex"
+              flexDirection="row"
+              alignItems="center"
+              justifyContent="center"
+              backgroundColor="#F6E2EE"
+              borderColor="#D1338A"
+              borderWidth={1}
+            >
+              <Text color="#D1338A">Causes</Text>
+              <IconButton
+                icon={
+                  <Icon
+                    as={MaterialIcons}
+                    name={
+                      iconState.Causes
+                        ? 'keyboard-arrow-up'
+                        : 'keyboard-arrow-down'
+                    }
+                    size={5}
+                    color="#D1338A"
+                  />
+                }
+                variant="outline"
+                _icon={{ color: 'gray.500' }}
+                onPress={() => handleTagPress('Causes')}
+              />
+            </Pressable>
+          </HStack>
+        </ScrollView>
       </VStack>
       <ScrollView>
         <VStack
